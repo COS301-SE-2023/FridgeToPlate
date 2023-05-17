71f1fcc281061c0e5a0ac28fc8dc62a1795d79fb
Antwi-Antwi <u21448150@tuks.co.za>
2023-05-17 09:05:36 +0200

    🎨 (app) Removed Duplicate Import

--- libs/app/core/src/core.module.ts
+++ libs/app/core/src/core.module.ts
@@ -3,9 +3,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { CoreShell } from "./core.shell";
 import { NxWelcomeComponent } from "./nx-welcome.component";
 import { CoreRouting } from "./core.routing";
-import { IonicModule } from "@ionic/angular";
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
 import { RouteReuseStrategy } from "@angular/router";
-import { IonicRouteStrategy } from '@ionic/angular';
 
 @NgModule({
   declarations: [CoreShell, NxWelcomeComponent],
