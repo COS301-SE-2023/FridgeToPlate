8b42bf0ab4b3d33709da38df1ef968c42dc8bf53
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-05-19 00:33:46 +0200

    🚧 (app) Profile Page Styling

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
-import { NxWelcomeComponent } from "./nx-welcome.component";
+import { ProfileModule } from "@fridge-to-plate/app/profile/feature";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ProfileModule],
   providers: [],
   bootstrap: [AppComponent],
 })
