6080aa76837abd41147d37679b14d31c9905e928
kphutiyagae <kopanophutiyagae@gmail.com>
2023-05-21 22:21:44 +0200

    feat(none): :sparkles: Added stepper form component.

--- libs/app/recipe-recommendation/src/app-recipe-recommendation.module.ts
+++ libs/app/recipe-recommendation/src/app-recipe-recommendation.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RecipeRecommendationPage } from "./components/RecipeRecommendationPage/recipe-recommendation-page";
 import {RecommendationRouting} from "./recommendation.routing";
 import {IonicModule} from "@ionic/angular";
+import {StepperForm} from "./components/stepper-form/stepper-form";
 
 @NgModule({
-  declarations: [RecipeRecommendationPage],
+  declarations: [RecipeRecommendationPage, StepperForm],
   imports: [CommonModule, RecommendationRouting, IonicModule],
 })
 export class AppRecipeRecommendationModule {}
---
+++ libs/app/recipe-recommendation/src/components/stepper-form/stepper-form.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-stepper-form',
+  templateUrl: './stepper-form.html',
+  styleUrls: ['./stepper-form.scss']
+})
+
+export class StepperForm {}
