17aacc664b9a71ecd2757aa25cc03940f674a2e3
kphutiyagae <kopanophutiyagae@gmail.com>
2023-05-21 21:36:07 +0200

    feat(none): :sparkles: Added recipe recommendation routing module

--- libs/app/recipe-recommendation/src/app-recipe-recommendation.module.ts
+++ libs/app/recipe-recommendation/src/app-recipe-recommendation.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeRecommendationPage } from "./components/RecipeRecommendationPage/recipe-recommendation-page";
+import {RecommendationRouting} from "./recommendation.routing";
 
 @NgModule({
   declarations: [RecipeRecommendationPage],
-  imports: [CommonModule],
+  imports: [CommonModule, RecommendationRouting],
 })
 export class AppRecipeRecommendationModule {}
