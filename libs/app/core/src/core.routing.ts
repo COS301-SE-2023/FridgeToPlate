de208bf8ccbd4e91043914cd16a1abb41bd3dbf8
kphutiyagae <kopanophutiyagae@gmail.com>
2023-05-21 19:21:42 +0200

    feat(none): :sparkles: Added recipe recommendation page.

--- libs/app/core/src/core.routing.ts
+++ libs/app/core/src/core.routing.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import {
+  RecipeRecommendationPage
+} from "../../recipe-recommendation/src/components/RecipeRecommendationPage/recipe-recommendation-page";
 
 const routes: Routes = [
     {
         path: '',
         pathMatch: 'full',
         component: NxWelcomeComponent,
-    },
-
+    }, { path: 'recommendations', component: RecipeRecommendationPage}
 ];
 
 @NgModule({
--- libs/app/recipe-recommendation/src/app-recipe-recommendation.module.ts
+++ libs/app/recipe-recommendation/src/app-recipe-recommendation.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RecipeRecommendationPage } from "./components/RecipeRecommendationPage/recipe-recommendation-page";
 
 @NgModule({
+  declarations: [RecipeRecommendationPage],
   imports: [CommonModule],
 })
 export class AppRecipeRecommendationModule {}
