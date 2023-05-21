eb4fef30ae26ec079f8544402031a870d812abf0
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-05-21 22:39:59 +0200

    🚧 (app) Profile Page Design and Implementation

--- libs/app/profile/feature/src/profile.page.ts
+++ libs/app/profile/feature/src/profile.page.ts
@@ -5,4 +5,60 @@ import { Component } from "@angular/core";
   templateUrl: "./profile.page.html",
   styleUrls: ["./profile.page.scss"],
 })
-export class ProfilePage {}
+export class ProfilePage {
+
+  subpage = "saved";
+  recipes = [
+    {
+      name: "Shrimp Pasta",
+      difficulty: "Medium",
+      tags: ["Seafood", "Pasta"]
+    },
+    {
+      name: "Pizza",
+      difficulty: "Easy",
+      tags: ["Italian", "Pizza"]
+    },
+    {
+      name: "Mushroom Pie",
+      difficulty: "Medium",
+      tags: ["Quick"]
+    },
+    {
+      name: "Beef Stew",
+      difficulty: "Easy",
+      tags: ["Winter", "Hearty"]
+    },
+  ]
+
+  ingredients = [
+    {
+      name: "Tomato",
+      amount: "3"
+    },
+    {
+      name: "Cucumber",
+      amount: "1"
+    },
+    {
+      name: "Beef",
+      amount: "200g"
+    },
+    {
+      name: "Stock",
+      amount: "500ml"
+    },
+  ]
+  
+  displayIngredients() {
+    this.subpage = "ingredients";
+  }
+
+  displaySaved() {
+    this.subpage = "saved";
+  }
+
+  removeIngredient(ingredientID: number) {
+    this.ingredients.splice(ingredientID, 1);
+  }
+}
