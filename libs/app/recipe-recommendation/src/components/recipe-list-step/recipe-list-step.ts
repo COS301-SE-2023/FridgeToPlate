9731048dbdde1ada38a0d2b872217027685a15fd
kphutiyagae <kopanophutiyagae@gmail.com>
2023-05-22 18:58:11 +0200

    feat(none): :construction: Added Recipe Preferences and list steps.

---
+++ libs/app/recipe-recommendation/src/components/recipe-list-step/recipe-list-step.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list-step',
+  templateUrl: './recipe-list-step.html',
+  styleUrls: ['recipe-list-step.scss']
+})
+export class RecipeListStep {
+
+  constructor() {
+  }
+}
---
+++ libs/app/recipe-recommendation/src/components/recipe-preferences-step/recipe-preferences-step.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-preferences-step',
+  templateUrl: './recipe-preferences-step.html',
+  styleUrls: ['recipe-preferences-step.scss']
+})
+export class RecipePreferencesStep {
+
+  constructor() {
+  }
+}
