91440d1cc456facf8a8bbc149cfa620b773a2e25
simphiwe-nonabe <106514948+simphiwe-nonabe@users.noreply.github.com>
2023-05-22 21:04:04 +0200

    ✨ (app) signup page component created

---
+++ libs/app/signup/feature/src/signup.page.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "signup-page",
+  templateUrl: "./signup.page.html",
+  styleUrls: ["./signup.page.scss"],
+})
+export class SignupPage {}
