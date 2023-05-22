3f602a28150278a1905b71b29cad13f220c15103
simphiwe-nonabe <106514948+simphiwe-nonabe@users.noreply.github.com>
2023-05-22 07:56:18 +0200

    🎨 (app) signup page file structure created

---
+++ libs/app/signup/feature/src/lib/app-signup-feature.spec.ts
@@ -0,0 +1,7 @@
+import { appSignupFeature } from './app-signup-feature';
+
+describe('appSignupFeature', () => {
+  it('should work', () => {
+    expect(appSignupFeature()).toEqual('app-signup-feature');
+  });
+});
