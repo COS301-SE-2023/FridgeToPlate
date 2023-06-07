e959fceb17c12bc1b5deeaece395541b751deb74
kphutiyagae <kopanophutiyagae@gmail.com>
2023-06-07 21:05:04 +0200

    feat: :sparkles: Added Page component for recipe detail

---
+++ libs/app/recipe-detail/feature/src/lib/recipe-detail-page/recipe-detail-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fridge-to-plate-recipe-detail-page',
+  templateUrl: './recipe-detail-page.component.html',
+  styleUrls: ['./recipe-detail-page.component.css']
+})
+export class RecipeDetailPageComponent {
+
+}
