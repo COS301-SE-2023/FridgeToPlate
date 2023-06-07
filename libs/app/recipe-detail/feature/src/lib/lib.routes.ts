4bd700df34ef82d8e1bd7c2a3b7f4d5592756804
kphutiyagae <kopanophutiyagae@gmail.com>
2023-06-07 21:30:48 +0200

    feat(none): :construction:
    Fixed lazy loading for recipe detail page.

--- libs/app/recipe-detail/feature/src/lib/lib.routes.ts
+++ libs/app/recipe-detail/feature/src/lib/lib.routes.ts
@@ -2,6 +2,6 @@ import { Route } from '@angular/router';
 import {RecipeDetailPageComponent} from "./recipe-detail-page/recipe-detail-page.component";
 
 export const appRecipeDetailFeatureRoutes: Route[] = [
-  /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
-  {path: '', component: RecipeDetailPageComponent}
+  {path: ':id', component: RecipeDetailPageComponent}
+
 ];
