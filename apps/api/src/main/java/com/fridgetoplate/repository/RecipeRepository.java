7a167313024e20a9098f852d55ba86b9063e9172
Antwi-Antwi <u21448150@tuks.co.za>
2023-06-09 13:02:48 +0200

    🚧 (Database) : Models created

--- apps/api/src/main/java/com/fridgetoplate/repository/RecipeRepository.java
+++ apps/api/src/main/java/com/fridgetoplate/repository/RecipeRepository.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
 import com.fridgetoplate.model.Recipe;
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
