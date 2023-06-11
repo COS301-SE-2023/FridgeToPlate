8d8e03e4a95e9cc2968ad7e6003371ff237b994a
kphutiyagae <kopanophutiyagae@gmail.com>
2023-06-11 15:06:28 +0200

    feat(none): :construction:
    Implemented IRecipe, Ingredient and Step interfaces.

--- models/ingredient.ts
+++ models/ingredient.ts
@@ -1,2 +1,10 @@
 export interface Ingredient {
+  id: string;
+  name: string;
+  tags?: string[];
+}
+
+export interface UserIngredient extends Ingredient {
+  quantity?: number;
+  mass?: number;
 }
