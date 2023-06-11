4b6bfbee9f17bf9c1600f5a13db77c1d76acfef1
kphutiyagae <kopanophutiyagae@gmail.com>
2023-06-11 14:52:47 +0200

    feat(none): :construction:
    Generated Recipe Interface for app and added to info card component.

--- libs/app/recipe-detail/ui/src/lib/recipe-info-card/recipe-info-card.component.ts
+++ libs/app/recipe-detail/ui/src/lib/recipe-info-card/recipe-info-card.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {IRecipe} from "../../../../../../../models/irecipe";
 
 @Component({
   selector: 'fridge-to-plate-recipe-info-card',
   templateUrl: './recipe-info-card.component.html',
   styleUrls: ['./recipe-info-card.component.css'],
 })
-export class RecipeInfoCardComponent {}
+export class RecipeInfoCardComponent {
+  @Input() recipe: IRecipe | undefined;
+}
