ebc3f6c2eee72cde3c71dc4914bf8967caf0478f
kphutiyagae <kopanophutiyagae@gmail.com>
2023-06-11 14:45:14 +0200

    feat(none): :construction:
    Generated Recipe Info Card.

--- libs/app/recipe-detail/ui/src/index.ts
+++ libs/app/recipe-detail/ui/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/app-recipe-detail-ui.module';
+
+export * from './lib/recipe-info-card/recipe-info-card.component';
---
+++ libs/app/recipe-detail/ui/src/lib/recipe-info-card/recipe-info-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fridge-to-plate-recipe-info-card',
+  templateUrl: './recipe-info-card.component.html',
+  styleUrls: ['./recipe-info-card.component.css'],
+})
+export class RecipeInfoCardComponent {}
