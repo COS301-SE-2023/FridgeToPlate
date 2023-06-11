a49c1a65b2bc87437535ff3db817c3487cfb4be2
kphutiyagae <kopanophutiyagae@gmail.com>
2023-06-11 14:45:14 +0200

    feat(none): :construction:
    Generated Recipe Info Card.

--- libs/app/recipe-detail/feature/src/lib/app-recipe-detail-feature.module.ts
+++ libs/app/recipe-detail/feature/src/lib/app-recipe-detail-feature.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { appRecipeDetailFeatureRoutes } from './lib.routes';
 import {RecipeDetailPageComponent} from "./recipe-detail-page/recipe-detail-page.component";
+import {AppRecipeDetailUiModule} from "@fridge-to-plate/app/recipe-detail/ui";
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(appRecipeDetailFeatureRoutes),
     RouterModule.forChild(appRecipeDetailFeatureRoutes),
+    AppRecipeDetailUiModule
   ],
   declarations: [RecipeDetailPageComponent]
 })
--- libs/app/recipe-detail/ui/src/lib/app-recipe-detail-ui.module.ts
+++ libs/app/recipe-detail/ui/src/lib/app-recipe-detail-ui.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RecipeInfoCardComponent } from './recipe-info-card/recipe-info-card.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [RecipeInfoCardComponent],
+  exports: [RecipeInfoCardComponent],
 })
 export class AppRecipeDetailUiModule {}
