9072f44576f695e574766b8f699071294c66b205
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-06-13 16:17:46 +0200

    🚧 (app) Setup Routing

--- libs/app/edit-profile/feature/src/edit-profile.page.ts
+++ libs/app/edit-profile/feature/src/edit-profile.page.ts
@@ -1,8 +1,16 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "edit-profile-page",
   templateUrl: "./edit-profile.page.html",
   styleUrls: ["./edit-profile.page.scss"],
 })
-export class EditProfilePage {}
+export class EditProfilePage {
+  
+  constructor(private router: Router) {}
+
+  closeSettings() {
+    this.router.navigate(['/profile']);
+  }
+}
