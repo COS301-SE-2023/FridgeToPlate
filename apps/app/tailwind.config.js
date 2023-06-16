d6674e783fd9aeb1a26e66c6e28d3489020018a1
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-06-16 11:10:59 +0200

    💄 (app) Added Save Button

--- apps/app/tailwind.config.js
+++ apps/app/tailwind.config.js
@@ -9,20 +9,11 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
+      'sm': '200px',
+      'md': '740px',
       'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
       'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
       '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
     },
     extend: {
       colors: {
