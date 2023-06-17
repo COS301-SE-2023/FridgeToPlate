026617b29c4d3f4c41be61266718f3bbc259d597
kphutiyagae <kopanophutiyagae@gmail.com>
2023-06-17 23:04:32 +0200

    feat(none): :refactor:
    Fixed user feedback code for recipe

--- libs/app/recipe/feature/src/recipe.page.ts
+++ libs/app/recipe/feature/src/recipe.page.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { recipeList } from "@fridge-to-plate/app/recipe/data-access";
 import { IRecipe} from "@fridge-to-plate/app/recipe/utils";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'recipe-page',
@@ -9,8 +10,18 @@ import { IRecipe} from "@fridge-to-plate/app/recipe/utils";
 })
 export class RecipePage implements OnInit {
   recipe: IRecipe = recipeList[0];
-
+  recipeId: Number | undefined;
+  constructor(private route: ActivatedRoute, private router: Router) {
+  }
   ngOnInit() {
-    console.log('Hello from here')
+    this.recipeId = Number.parseInt(this.route.snapshot.paramMap.get('id') ?? "");
+
+    if(Number.isNaN(this.recipeId)){
+      this.router.navigate(['/recipe/'])
+    }
+
+    else {
+      //Use API
+    }
   }
 }
