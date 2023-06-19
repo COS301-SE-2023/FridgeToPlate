d2e0b29690d1dea86512dfd16bf3c4fafa40a35f
kphutiyagae <kopanophutiyagae@gmail.com>
2023-06-19 19:54:34 +0200

    fix: :construction: Fixed HttpClient injector error]

--- libs/app/recipe/feature/src/recipe.page.ts
+++ libs/app/recipe/feature/src/recipe.page.ts
@@ -1,35 +1,50 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {RecipeDetailApiService} from "../../data-access/src/lib/recipe-detail-api.service";
-import {Observable, switchMap} from "rxjs";
-import {IRecipe} from "@fridge-to-plate/app/recipe/utils";
-import {fromFetch} from "rxjs/internal/observable/dom/fetch";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RecipeDetailApiService } from '../../data-access/src/lib/recipe-detail-api.service';
+import { Observable, switchMap } from 'rxjs';
+import { IRecipe } from '@fridge-to-plate/app/recipe/utils';
+import { fromFetch } from 'rxjs/internal/observable/dom/fetch';
 
 @Component({
   selector: 'recipe-page',
   templateUrl: './recipe.page.html',
-  styleUrls: ['./recipe.page.scss']
+  styleUrls: ['./recipe.page.scss'],
 })
-export class RecipePage implements OnInit {
-
+export class RecipePage {
   recipeId: Number | undefined;
-  recipe: Observable<IRecipe> | undefined;
+  recipe$: Observable<IRecipe> | undefined;
 
-  constructor(private route: ActivatedRoute,
-              private router: Router,
-              private recipeApiService: RecipeDetailApiService
-  ) {}
-  ngOnInit() {
-    this.recipeId = Number.parseInt(this.route.snapshot.paramMap.get('id') ?? "");
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private recipeApiService: RecipeDetailApiService
+  ) {
+    this.recipeId = Number.parseInt(
+      this.route.snapshot.paramMap.get('id') ?? ''
+    );
 
-    if(Number.isNaN(this.recipeId)){
-      this.router.navigate(['/recipe/'])
+    if (Number.isNaN(this.recipeId)) {
+      this.router.navigate(['/recipe/']);
     }
 
-    else {
-      //Use API
-      this.recipe = this.recipeApiService
-        .getRecipeDetails(this.recipeId.valueOf())
-    }
+    this.recipe$ = route.paramMap.pipe(
+      switchMap((params) =>
+        this.recipeApiService.getRecipeDetails(Number(params.get('id')))
+      )
+    );
   }
+
+  // ngOnInit() {
+  //   this.recipeId = Number.parseInt(this.route.snapshot.paramMap.get('id') ?? "");
+
+  //   if(Number.isNaN(this.recipeId)){
+  //     this.router.navigate(['/recipe/'])
+  //   }
+
+  //   else {
+  //     //Use API
+  //     this.recipe$ = this.recipeApiService
+  //       .getRecipeDetails(this.recipeId.valueOf())
+  //   }
+  // }
 }
