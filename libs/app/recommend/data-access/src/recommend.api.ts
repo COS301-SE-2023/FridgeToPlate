940577831c6c85f296143e238f2ba8f19e14ae1e
kphutiyagae <kopanophutiyagae@gmail.com>
2023-06-20 19:43:10 +0200

    feat(none): :sparkles: Added API Service

--- libs/app/recommend/data-access/src/recommend.api.ts
+++ libs/app/recommend/data-access/src/recommend.api.ts
@@ -1,2 +1,27 @@
 import {ingredientsArray} from './ingredients.mock';
+import {IRecipe} from "@fridge-to-plate/app/recipe/utils";
+import {IIngredient} from "@fridge-to-plate/app/ingredient/utils";
 
+export interface Interface {
+  ingredients: IIngredient[];
+  diet: string[];
+  meta?: {
+    difficulty?: 'easy' | 'medium' | 'hard';
+    rating?: number;
+    numberOfServings?: number;
+  }
+}
+export class RecommendApi {
+
+  //Step 1
+  getUserngredientsList(): IRecipe[] {
+  }
+  removeIngredient(recipe: IRecipe){
+  }
+
+  //Step 2
+  getDietList(): string [] {}
+
+  //Step 3
+  getRecommendations(recomendationParams: )
+}
