38e4b2816116a7c7681592316eff663c76a4e4bb
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-06-24 13:52:20 +0200

    🐛 (app) Jest Tests

--- libs/app/ingredient/ui/src/ingredient-card/ingredient-card.component.spec.ts
+++ libs/app/ingredient/ui/src/ingredient-card/ingredient-card.component.spec.ts
@@ -1,10 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IngredientCardComponent } from './ingredient-card.component';
 import { IonicModule } from '@ionic/angular';
+import { IIngredient } from '@fridge-to-plate/app/ingredient/utils';
 
 describe('IngredientCardComponent', () => {
   let component: IngredientCardComponent;
   let fixture: ComponentFixture<IngredientCardComponent>;
+  let testIngredient: IIngredient;
+
+  testIngredient = {
+    id: "test-id",
+    name: "Carrot"
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,6 +21,7 @@ describe('IngredientCardComponent', () => {
 
     fixture = TestBed.createComponent(IngredientCardComponent);
     component = fixture.componentInstance;
+    component.ingredient = testIngredient;
     fixture.detectChanges();
   });
 
