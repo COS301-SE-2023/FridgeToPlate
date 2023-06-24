19ca85fb22de02efd28a29d4dbb920530861b7e6
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-06-24 17:06:59 +0200

    ✨ (app) Added Edit Profile Modal

---
+++ libs/app/profile/ui/src/edit-modal/edit-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'edit-modal',
+  templateUrl: './edit-modal.component.html',
+  styleUrls: ['./edit-modal.component.css'],
+})
+export class EditModalComponent {}
