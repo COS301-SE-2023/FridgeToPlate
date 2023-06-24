7a43764b6728f62efe89a91687d27c6ba488d3bb
simphiwe-nonabe <106514948+simphiwe-nonabe@users.noreply.github.com>
2023-06-24 14:46:23 +0200

    ✨ (app) navbar introduced

--- libs/app/navigation/feature/src/navigation.component.ts
+++ libs/app/navigation/feature/src/navigation.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'navigation',
+  selector: 'navigation-bar',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
