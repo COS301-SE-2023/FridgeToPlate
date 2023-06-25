a801423634b9339cd3862004520891149a3ea0d7
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-06-25 11:03:13 +0200

    🚧 (app) Added Edit Modal

--- libs/app/profile/ui/src/edit-modal/edit-modal.component.ts
+++ libs/app/profile/ui/src/edit-modal/edit-modal.component.ts
@@ -1,8 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'edit-modal',
   templateUrl: './edit-modal.component.html',
   styleUrls: ['./edit-modal.component.css'],
 })
-export class EditModalComponent {}
+export class EditModalComponent {
+  @Output() closeFunc: EventEmitter<any> = new EventEmitter();
+  
+  close() {
+    this.closeFunc.emit();
+  }
+}
