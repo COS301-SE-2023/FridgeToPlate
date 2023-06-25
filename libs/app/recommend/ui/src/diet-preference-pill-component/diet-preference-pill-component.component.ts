0342556824c540e850ddee8fbd88742ef2852582
kphutiyagae <kopanophutiyagae@gmail.com>
2023-06-25 02:29:25 +0200

    feat: :sparkles: Added Pill component

---
+++ libs/app/recommend/ui/src/diet-preference-pill-component/diet-preference-pill-component.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fridge-to-plate-diet-preference-pill-component',
+  templateUrl: './diet-preference-pill-component.component.html',
+  styleUrls: ['./diet-preference-pill-component.component.css'],
+})
+export class DietPreferencePillComponentComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
