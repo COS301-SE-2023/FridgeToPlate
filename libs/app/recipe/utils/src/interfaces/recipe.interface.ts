a789d5861696366be431d0b3790508cd33de3163
Antwi-Antwi <u21448150@tuks.co.za>
2023-06-26 04:22:41 +0200

    🐛 (app) : restored recipe interface

--- libs/app/recipe/utils/src/interfaces/recipe.interface.ts
+++ libs/app/recipe/utils/src/interfaces/recipe.interface.ts
@@ -1,7 +1,17 @@
+import { IIngredient } from '@fridge-to-plate/app/ingredient/utils';
+import { IRecipeStep } from './recipe-step.interface';
 
 export interface IRecipe {
-    recipeId: string;
-    name: string;
-    ingredients: IIngredient[];
-    instructions: String[];
+  id: string;
+  name: string;
+  recipeImage: string;
+  ingredients: IIngredient[];
+  steps: IRecipeStep[];
+  rating?: number;
+  difficulty: 'easy' | 'medium' | 'hard';
+  meta?: {
+    prepTime?: number;
+    numberOfServings?: number;
+    tags: string[];
+  };
 }
