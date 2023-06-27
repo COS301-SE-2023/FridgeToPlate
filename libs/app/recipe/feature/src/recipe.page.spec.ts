24a71fc7a3c2f67f6a31223cd0991c9d6b0fef10
Azola <azola.m.lukhozi@gmail.com>
2023-06-27 11:28:51 +0200

    ✅ (App) Added Testing for Recipe Details

--- libs/app/recipe/feature/src/recipe.page.spec.ts
+++ libs/app/recipe/feature/src/recipe.page.spec.ts
@@ -7,8 +7,13 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { RecipeUIModule } from '@fridge-to-plate/app/recipe/ui';
 import { NavigationBarModule } from '@fridge-to-plate/app/navigation/feature';
 import { IRecipe } from '@fridge-to-plate/app/recipe/utils';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { Location } from '@angular/common';
+import { RecipeService } from '@fridge-to-plate/app/recipe/data-access';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 describe('RecipeDetailPageComponent', () => {
+  let location: Location;
+  let recipeService: RecipeService;
   let component: RecipePage;
   let fixture: ComponentFixture<RecipePage>;
   let testRecipe: IRecipe = {
@@ -31,15 +36,38 @@ describe('RecipeDetailPageComponent', () => {
     fixture = TestBed.createComponent(RecipePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    location = TestBed.inject(Location);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+
   it('should observe recipe details', () => {
     jest.spyOn(component, 'setRecipe').mockImplementation((id: string) => component.recipe = testRecipe);
     component.setRecipe("test-id");
     expect(component.recipe).toEqual(testRecipe);
   });
+
+
+  it('should navigate back when goBack() is called', () => {
+    const locationSpy = jest.spyOn(location, 'back');
+    component.goBack();
+    expect(locationSpy).toHaveBeenCalled();
+  });
+
+
+  it('should initialize the component and call setRecipe() with the correct id', () => {
+    const paramMap = convertToParamMap({ id: 'test-id' });
+    const route: ActivatedRoute = TestBed.inject(ActivatedRoute);
+    jest.spyOn(route, 'paramMap', 'get').mockReturnValue(of(paramMap));
+
+    const setRecipeSpy = jest.spyOn(component, 'setRecipe');
+    component.ngOnInit();
+
+    expect(setRecipeSpy).toHaveBeenCalledWith('test-id');
+  });
+
+
 });
