8e51ee598b06c8653af4c6b5fe1f6fdbefbf043b
kphutiyagae <kopanophutiyagae@gmail.com>
2023-06-27 10:32:02 +0200

    Added cypress tests

--- apps/app-e2e/src/e2e/app.cy.ts
+++ apps/app-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,7 @@
+import { data } from 'cypress/types/jquery';
 import { getGreeting } from '../support/app.po';
 
-describe('login', () => {
+describe('login tests', () => {
   beforeEach(() => cy.visit('/'));
 
   it('successfully loads login', () => {
@@ -8,7 +9,61 @@ describe('login', () => {
   });
 
   it('should prevent incorrect login attempt', () => {
-    cy.get('input[name="username"]').type('12344321');
-    cy.get('input[name="password"]').type('12344321');
+    cy.fixture('user-details.json').then((userData) => {
+      cy.get('input[name="username"]').type(userData.username);
+      cy.get('input[name="password"]').type(userData.password);
+      cy.get('button').click();
+      cy.url().contains('profile');
+    });
   });
+
+  it('should attempt login', () => {
+    cy.get('input[name="username"]').type('smileyazola@gmail.com');
+    cy.get('input[name="password"]').type('randomPassword!');
+    cy.get('button').click();
+    cy.url().contains('profile');
+  });
+
+  it('should navigate to signup', () => {
+    cy.get('button').click();
+    cy.url().contains('signup');
+  });
+});
+
+describe('signup tests', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('successfully loads signup', () => {
+    cy.get('h1').contains('Hey, Welcome Back');
+  });
+
+  it('should attempt signup', () => {
+    cy.fixture('user-details.json').then((userData) => {
+      cy.get('input[name="username"]').type(userData.username);
+      cy.get('input[name="password"]').type(userData.password);
+      cy.get('button').click();
+      cy.url().contains('profile');
+    });
+  });
+
+  it('should navigate to login', () => {
+    cy.get('button').click();
+    cy.url().contains('login');
+  });
+});
+
+describe('profile tests', () => {
+  beforeEach(() => cy.visit('/'));
+});
+
+describe('create tests', () => {
+  beforeEach(() => cy.visit('/'));
+});
+
+describe('generate tests', () => {
+  beforeEach(() => cy.visit('/'));
+});
+
+describe('details tests', () => {
+  beforeEach(() => cy.visit('/'));
 });
