df4f535b5ce6b89cf81be363d15bfb3322302a30
Antwi-Antwi <u21448150@tuks.co.za>
2023-07-02 18:27:33 +0200

    ⚰️ (api) : removed duplicate lines

--- apps/api/src/main/java/com/fridgetoplate/api/IngredientController.java
+++ apps/api/src/main/java/com/fridgetoplate/api/IngredientController.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
