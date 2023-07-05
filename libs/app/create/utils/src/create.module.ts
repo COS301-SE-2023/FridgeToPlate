befdb209a985e246cc31588792291a9828cbf2a2
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-07-05 11:46:48 +0200

    ✨(app) Added NGXS for Recipe Creation

--- libs/app/create/utils/src/create.module.ts
+++ libs/app/create/utils/src/create.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CreateAPI } from './create.api';
 
 @NgModule({
   imports: [CommonModule],
 })
-export class AppCreateDataAccessModule {}
+export class CreateUtilsModule {}
