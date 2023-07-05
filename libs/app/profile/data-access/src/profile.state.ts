6519d82eb9224b7eeb2b80653610603b16c61425
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-07-05 12:14:14 +0200

    ✨(app) Added NGXS Commands Recipe Creation

--- libs/app/profile/data-access/src/profile.state.ts
+++ libs/app/profile/data-access/src/profile.state.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { IProfile, UpdateProfile } from "@fridge-to-plate/app/profile/utils";
 import { Action, Selector, State, StateContext } from "@ngxs/store";
+import { ProfileAPI } from "./profile.api";
 
 export interface ProfileStateModel {
     profile: IProfile | null;
@@ -24,6 +25,8 @@ export interface ProfileStateModel {
 
 @Injectable()
 export class ProfileState {
+
+    constructor(private api: ProfileAPI) {}
     
     @Selector()
     static getProfile(state: ProfileStateModel) {
@@ -34,6 +37,7 @@ export class ProfileState {
     updateProfile({ patchState } : StateContext<ProfileStateModel>, { profile } : UpdateProfile) {
         patchState({
             profile: profile
-        })
+        });
+        this.api.updateProfile(profile);
     }
 }
