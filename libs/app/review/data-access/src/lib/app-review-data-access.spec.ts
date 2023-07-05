cd0e80fc04b6c016241513f5090c337378defeb7
simphiwe-nonabe <106514948+simphiwe-nonabe@users.noreply.github.com>
2023-07-05 21:13:48 +0200

    ✨ (app) review data-access library added

---
+++ libs/app/review/data-access/src/lib/app-review-data-access.spec.ts
@@ -0,0 +1,7 @@
+import { appReviewDataAccess } from './app-review-data-access';
+
+describe('appReviewDataAccess', () => {
+  it('should work', () => {
+    expect(appReviewDataAccess()).toEqual('app-review-data-access');
+  });
+});
