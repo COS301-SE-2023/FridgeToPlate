c94ab04ff325f4e8916d6a6eeb3a16458489222a
simphiwe-nonabe <106514948+simphiwe-nonabe@users.noreply.github.com>
2023-07-05 21:10:20 +0200

    ✨ (app) review feature library added

---
+++ libs/app/review/feature/src/lib/app-review-feature.spec.ts
@@ -0,0 +1,7 @@
+import { appReviewFeature } from './app-review-feature';
+
+describe('appReviewFeature', () => {
+  it('should work', () => {
+    expect(appReviewFeature()).toEqual('app-review-feature');
+  });
+});
