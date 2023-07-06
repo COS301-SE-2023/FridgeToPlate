321441b6e8c300fb10437c4f58785173d39883d5
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-06 23:42:47 +0200

    feat: :sparkles: Added page component for notifications page.

--- libs/app/notifications/feature/src/lib/app-notifications-feature.module.ts
+++ libs/app/notifications/feature/src/lib/app-notifications-feature.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotificationsPageComponent } from './notifications-page/notifications-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    NotificationsPageComponent
+  ],
 })
 export class AppNotificationsFeatureModule {}
