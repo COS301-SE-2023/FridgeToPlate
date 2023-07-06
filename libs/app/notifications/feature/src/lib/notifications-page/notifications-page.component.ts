e5cd02ac3456b74068ad003b353ea9d23b926bdb
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-06 23:42:47 +0200

    feat: :sparkles: Added page component for notifications page.

---
+++ libs/app/notifications/feature/src/lib/notifications-page/notifications-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fridge-to-plate-notifications-page',
+  templateUrl: './notifications-page.component.html',
+  styleUrls: ['./notifications-page.component.css']
+})
+export class NotificationsPageComponent {
+
+}
