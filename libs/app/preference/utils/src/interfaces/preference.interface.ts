cd31b27abd27f4dac05738a42a797171a7c7a07e
Azola <azola.m.lukhozi@gmail.com>
2023-07-06 21:03:36 +0200

    🔧 (App) Cleaned And Finished Preference Utils

---
+++ libs/app/preference/utils/src/interfaces/preference.interface.ts
@@ -0,0 +1,8 @@
+export interface IPreference {
+  userId?: string;
+  username: string;
+  darkMode: boolean;
+  recommendNotification: boolean;
+  viewsNotification: boolean;
+  reviewsdNotification: boolean;
+}
