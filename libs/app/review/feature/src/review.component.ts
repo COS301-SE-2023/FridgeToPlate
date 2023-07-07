5caf750035a945febbfc8ac483d3b933ce077c03
simphiwe-nonabe <106514948+simphiwe-nonabe@users.noreply.github.com>
2023-07-07 13:07:31 +0200

    ✨ (app) review section added

--- libs/app/review/feature/src/review.component.ts
+++ libs/app/review/feature/src/review.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'navigation-bar',
+  selector: 'review',
   templateUrl: './review.html',
   styleUrls: ['./review.scss'],
 })
 export class Review {
+  rating = 0;
+  description = '';
 
+  setRating(num: number) {
+    this.rating = num;
+  }
+
+  submitReview() {
+    if (this.rating === 0) {
+      alert('Please rate the recipe before submitting your review!');
+      return;
+    }
+
+    const review = {
+      rating: this.rating,
+      description: this.description
+    };
+
+    // send the review data to a server or store it locally
+    console.log(review);
+  }
 }
--- libs/app/review/feature/src/review.module.ts
+++ libs/app/review/feature/src/review.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Review } from './review.component';
 import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    FormsModule
   ],
   declarations: [Review],
   exports: [Review]
