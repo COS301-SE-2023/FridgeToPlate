31ddd025c38a65384706ede2995af268423472b6
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-09 00:14:52 +0200

    test: :test_tube: Added route guard tests.

--- libs/app/core/src/guards/route-guard.service.spec.ts
+++ libs/app/core/src/guards/route-guard.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RouteGuardService } from './route-guard.service';
+import { Router } from '@angular/router';
 
 describe('RouteGuardService', () => {
   let service: RouteGuardService;
+  let navigateSpy: jest.SpyInstance;
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(RouteGuardService);
+    router = TestBed.inject(Router);
+    navigateSpy = jest.spyOn(router, 'createUrlTree');
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  // it('should remain on same route (No Login)', () => {
+  //   expect(service).toBeTruthy();
+  //   service.canActivate();
+  //   expect(navigateSpy).toHaveBeenCalledWith(['/login']);
+  // });
+
+  it('should navigate to all routes (Login provided)', () => {
+    expect(service).toBeTruthy();
+    expect(service.canActivate()).toBe(true);
+  });
+
 });
