a4693a04cffc1aff9873a8ff37a316df3b95c411
simphiwe-nonabe <106514948+simphiwe-nonabe@users.noreply.github.com>
2023-07-08 22:41:14 +0200

    ✅ (app) Unit tests added

--- libs/app/review/feature/src/review.spec.ts
+++ libs/app/review/feature/src/review.spec.ts
@@ -1,7 +1,43 @@
-import { appReviewFeature } from './review';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Review } from './review.component';
 
-describe('appReviewFeature', () => {
-  it('should work', () => {
-    expect(appReviewFeature()).toEqual('app-review-feature');
+describe('Review', () => {
+  let component: Review;
+  let fixture: ComponentFixture<Review>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [Review]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Review);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should set the rating property', () => {
+    component.setRating(3);
+    expect(component.rating).toEqual(3);
+  });
+
+  it('should toggle the showReviews property', () => {
+    component.toggleReviews();
+    expect(component.showReviews).toBe(true);
+
+    component.toggleReviews();
+    expect(component.showReviews).toBe(false);
+  });
+
+  it('should add a review to the reviews array', () => {
+    component.rating = 4;
+    component.description = 'Great recipe';
+
+    component.submitReview();
+
+    expect(component.reviews.length).toEqual(5);
+    expect(component.reviews[4].rating).toEqual(4);
+    expect(component.reviews[4].description).toEqual('Great recipe');
   });
 });
--- libs/app/review/feature/src/review.ts
+++
@@ -1,3 +0,0 @@
-export function appReviewFeature(): string {
-  return 'app-review-feature';
-}
