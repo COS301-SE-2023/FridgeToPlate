b78bad9fc2517895dc73665d2bf3ed02fe76fe8f
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-09 20:30:11 +0200

    feat: :construction: WIP: (Finally) Fixed NG Zorro import error

--- libs/app/notifications/feature/src/lib/app-notifications-feature.module.ts
+++ libs/app/notifications/feature/src/lib/app-notifications-feature.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotificationsPageComponent } from './notifications-page/notifications-page.component';
 import { NzListModule } from 'ng-zorro-antd/list';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
 
 @NgModule({
-  imports: [CommonModule, RoutingMo],
+  imports: [CommonModule, NzListModule, NzTabsModule],
   declarations: [
     NotificationsPageComponent
   ],
+  exports: [ NotificationsPageComponent ]
 })
 export class AppNotificationsFeatureModule {}
