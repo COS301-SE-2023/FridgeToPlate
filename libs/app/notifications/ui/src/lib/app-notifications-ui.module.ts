ae618374c279ee17b024a09f802c0a2c4cbdaabf
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-10 22:55:03 +0200

    feat: :sparkles: Added tab child component

--- libs/app/notifications/ui/src/lib/app-notifications-ui.module.ts
+++ libs/app/notifications/ui/src/lib/app-notifications-ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabbedComponent } from './tabbed/tabbed.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [TabbedComponent],
+  declarations: [TabbedComponent, TabComponent],
   exports: [TabbedComponent],
 })
 export class AppNotificationsUiModule {}
---
+++ libs/app/notifications/ui/src/lib/tab/tab.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fridge-to-plate-tab',
+  templateUrl: './tab.component.html',
+  styleUrls: ['./tab.component.css'],
+})
+export class TabComponent {}
