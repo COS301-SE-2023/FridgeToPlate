5428db558e589de023d7471e1e8d25042d4b4159
simphiwe-nonabe <106514948+simphiwe-nonabe@users.noreply.github.com>
2023-07-10 23:12:13 +0200

    pulled from dev

--- apps/app-e2e/src/e2e/app.cy.ts
+++ apps/app-e2e/src/e2e/app.cy.ts
@@ -4,53 +4,52 @@ import { getGreeting } from '../support/app.po';
 describe('login tests', () => {
   beforeEach(() => cy.visit('/'));
 
-  // it('successfully loads login', () => {
-  //   cy.get('h1').contains('Hey, Welcome Back');
-  // });
+  it('successfully loads login', () => {
+    cy.get('h1').contains('Hey, Welcome Back');
+  });
 
-  // it('should prevent incorrect login attempt', () => {
-  //   cy.fixture('user-details.json').then((userData) => {
-  //     cy.get('input[name="username"]').type(userData.username);
-  //     cy.get('input[name="password"]').type(userData.password);
-  //     cy.get('button').click();
-  //     cy.url().contains('profile');
-  //   });
-  // });
+  it('should prevent incorrect login attempt', () => {
+    cy.fixture('user-details.json').then((userData) => {
+      cy.get('input[name="username"]').type(userData.username);
+      cy.get('input[name="password"]').type(userData.password);
+      cy.get('button').click();
+      cy.url().contains('profile');
+    });
+  });
 
-  // it('should attempt login', () => {
-  //   cy.get('input[name="username"]').type('smileyazola@gmail.com');
-  //   cy.get('input[name="password"]').type('randomPassword!');
-  //   cy.get('button').click();
-  //   cy.url().contains('profile');
-  // });
+  it('should attempt login', () => {
+    cy.get('input[name="username"]').type('smileyazola@gmail.com');
+    cy.get('input[name="password"]').type('randomPassword!');
+    cy.get('button').click();
+    cy.url().contains('profile');
+  });
 
-  // it('should navigate to signup', () => {
-  //   cy.get('button').click();
-  //   cy.url().contains('signup');
-  // });
+  it('should navigate to signup', () => {
+    cy.get('button').click();
+    cy.url().contains('signup');
+  });
 });
 
 describe('signup tests', () => {
   beforeEach(() => cy.visit('/'));
 });
-// it('successfully loads signup', () => {
-//   cy.get('h1').contains('Hey, Welcome Back');
-// });
+it('successfully loads signup', () => {
+  cy.get('h1').contains('Hey, Welcome Back');
+});
 
-// it('should attempt signup', () => {
-//   cy.fixture('user-details.json').then((userData) => {
-//     cy.get('input[name="username"]').type(userData.username);
-//     cy.get('input[name="password"]').type(userData.password);
-//     cy.get('button').click();
-//     cy.url().contains('profile');
-//   });
-// });
+it('should attempt signup', () => {
+  cy.fixture('user-details.json').then((userData) => {
+    cy.get('input[name="username"]').type(userData.username);
+    cy.get('input[name="password"]').type(userData.password);
+    cy.get('button').click();
+    cy.url().contains('profile');
+  });
+});
 
-//   it('should navigate to login', () => {
-//     cy.get('button').click();
-//     cy.url().contains('login');
-//   });
-// });
+  it('should navigate to login', () => {
+    cy.get('button').click();
+    cy.url().contains('login');
+  });
 
 describe('profile tests', () => {
   beforeEach(() => cy.visit('/'));
