b73f42bf8c43d00cca2810428aef2e305c1082b8
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-11 00:29:53 +0200

    feat: :sparkles: Completed tab functionality

--- libs/app/notifications/ui/src/lib/tabbed/tabbed.component.ts
+++ libs/app/notifications/ui/src/lib/tabbed/tabbed.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ContentChildren,
+  QueryList,
+} from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'fridge-to-plate-tabbed',
   templateUrl: './tabbed.component.html',
   styleUrls: ['./tabbed.component.css'],
 })
-export class TabbedComponent {
-  selectTab(_t5: { category: string; count: number; active: boolean }) {
-    throw new Error('Method not implemented.');
+export class TabbedComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs!: QueryList<TabComponent>;
+
+  // tabs = [
+  //   { category: 'General', count: 8, active: true },
+  //   { category: 'Recommendations', count: 4, active: false },
+  // ];
+  ngAfterContentInit() {
+    console.log(this.tabs);
+    const activeTabs = this.tabs.filter((tab) => tab.active);
+
+    if (activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
   }
-  tabs = [
-    { category: 'General', count: 8, active: true },
-    { category: 'Recommendations', count: 4, active: false },
-  ];
+
+  selectTab(tab: TabComponent) {
+    this.tabs.toArray().forEach((tab) => (tab.active = false));
+    tab.active = true;
+  }
 }
