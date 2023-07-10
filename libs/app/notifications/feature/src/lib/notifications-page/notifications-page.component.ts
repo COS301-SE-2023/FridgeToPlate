5975b244ae68a3bc1b9556407c27f7078def3c39
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-11 01:48:41 +0200

    feat: :construction: WIP: Imported NotificationsAPI into page component and created observable.

--- libs/app/notifications/feature/src/lib/notifications-page/notifications-page.component.ts
+++ libs/app/notifications/feature/src/lib/notifications-page/notifications-page.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import {
+  INotification,
+  NotificationsApi,
+} from 'libs/app/notifications/data-access/src/notifications-api';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'fridge-to-plate-notifications-page',
   templateUrl: './notifications-page.component.html',
-  styleUrls: ['./notifications-page.component.css']
+  styleUrls: ['./notifications-page.component.css'],
 })
 export class NotificationsPageComponent {
-  tabs = [{ category: 'General', count: 8}, { category: 'Recommendations', count: 4}];
+  notifications$: Observable<INotification[]>;
+
+  tabs = [
+    { category: 'General', count: 8 },
+    { category: 'Recommendations', count: 4 },
+  ];
+
+  constructor(private notificationsApi: NotificationsApi) {
+    this.notifications$ = this.notificationsApi.getAllNotifications('');
+  }
 }
