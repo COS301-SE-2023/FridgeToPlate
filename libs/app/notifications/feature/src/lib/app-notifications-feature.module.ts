2834945f202ab342573a8666470e6e0c146057da
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-10 22:03:23 +0200

    feat: :sparkles: Added Tabbed component into feature module for notifications module

--- libs/app/notifications/feature/src/lib/app-notifications-feature.module.ts
+++ libs/app/notifications/feature/src/lib/app-notifications-feature.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NotificationsPageComponent } from './notifications-page/notifications-page.component';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { AppNotificationsUiModule } from 'libs/app/notifications/ui/src';
 
 @NgModule({
-  imports: [CommonModule, NzListModule, NzTabsModule],
-  declarations: [
-    NotificationsPageComponent
-  ],
-  exports: [ NotificationsPageComponent ]
+  imports: [CommonModule, NzListModule, NzTabsModule, AppNotificationsUiModule],
+  declarations: [NotificationsPageComponent],
+  exports: [NotificationsPageComponent],
 })
 export class AppNotificationsFeatureModule {}
