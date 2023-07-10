761ee70391ddc48978c6b8c84fa28690dc85fe6f
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-10 21:58:13 +0200

    feat: :sparkles: Added tabbed component to ui library

---
+++ libs/app/notifications/ui/src/lib/tabbed/tabbed.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fridge-to-plate-tabbed',
+  templateUrl: './tabbed.component.html',
+  styleUrls: ['./tabbed.component.css'],
+})
+export class TabbedComponent {}
