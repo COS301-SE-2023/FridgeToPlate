643c1ac5388c2479a3446cfe1a75af55383605f5
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-11 17:16:44 +0200

    fix: :white_check_mark: Fixed failing tests.

--- libs/app/profile/feature/src/profile.routing.ts
+++ libs/app/profile/feature/src/profile.routing.ts
@@ -3,15 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfilePage } from './profile.page';
 
 const routes: Routes = [
-    { 
-      path: '', 
-      pathMatch: 'full',
-      component: ProfilePage,
-    },
-    { 
-      path: 'notifications',
-      loadChildren: () => import('@fridge-to-plate/app/notifications/feature').then((m) => m.NotificationsFeatureModule),
-    }
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ProfilePage,
+  },
+  {
+    path: 'notifications',
+    loadChildren: () =>
+      import('@fridge-to-plate/app/notifications/feature').then(
+        (m) => m.NotificationsFeatureModule
+      ),
+  },
 ];
 
 @NgModule({
