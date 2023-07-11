0ef77fb6a441e32ed55ff9e82f87dc010aadde3e
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-11 02:13:29 +0200

    feat: :sparkles: Added observable code

--- libs/app/notifications/data-access/src/notifications-api.ts
+++ libs/app/notifications/data-access/src/notifications-api.ts
@@ -1,44 +1,49 @@
 import { BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
 
-
-export interface INotification { 
-    userName: string;
-    profilePictureUrl: string;
-    comment: string;
+export interface INotification {
+  userName: string;
+  profilePictureUrl: string;
+  comment: string;
+  recipeId: string;
 }
 
 @Injectable({
-    providedIn: 'root',
-  })
-  
-export class NotificationsApi{
+  providedIn: 'root',
+})
+export class NotificationsApi {
+  data: INotification[] = [
+    {
+      userName: 'Ryan T.',
+      profilePictureUrl:
+        'http://fastly.picsum.photos/id/201/400/400.jpg?hmac=FBmAp8pCOKUpdMCB7bMbP_Gnk_EFZscbJ96EoKV--aE',
+      comment: 'Interesting combination but we loved it at home.',
+      recipeId: 'b6df9e16-4916-4869-a7d9-eb0293142f1f',
+    },
+    {
+      userName: 'Paul P.',
+      profilePictureUrl:
+        'http://fastly.picsum.photos/id/201/400/400.jpg?hmac=FBmAp8pCOKUpdMCB7bMbP_Gnk_EFZscbJ96EoKV--aE',
+      comment: 'Good stuff!',
+      recipeId: 'b6df9e16-4916-4869-a7d9-eb0293142f1f',
+    },
+    {
+      userName: 'Azola L.',
+      profilePictureUrl:
+        'http://fastly.picsum.photos/id/201/400/400.jpg?hmac=FBmAp8pCOKUpdMCB7bMbP_Gnk_EFZscbJ96EoKV--aE',
+      comment: 'Tastes amazing, never knew you could flavor lamb like that!',
+      recipeId: 'b6df9e16-4916-4869-a7d9-eb0293142f1f',
+    },
+    {
+      userName: 'Simphiwe N.',
+      profilePictureUrl:
+        'http://fastly.picsum.photos/id/201/400/400.jpg?hmac=FBmAp8pCOKUpdMCB7bMbP_Gnk_EFZscbJ96EoKV--aE',
+      comment: 'Needs more salt',
+      recipeId: 'b6df9e16-4916-4869-a7d9-eb0293142f1f',
+    },
+  ];
 
-    data: INotification[] = [
-        {
-            userName: "Ryan T.",
-            profilePictureUrl: "https: //picsum.photos/400",
-            comment: "Random comment."
-        },
-        {
-            userName: "Paul P.",
-            profilePictureUrl: "https: //picsum.photos/400",
-            comment: "Random comment."
-        },
-        {
-            userName: "Azola L.",
-            profilePictureUrl: "https: //picsum.photos/400",
-            comment: "Random comment."
-        },
-        {
-            userName: "Simphiwe N.",
-            profilePictureUrl: "https: //picsum.photos/400",
-            comment: "Random comment."
-        }
-    ];
-
-    getAllNotifications(userId: string){
-        return new BehaviorSubject(this.data);
-    }
-
+  getAllNotifications(userId: string) {
+    return new BehaviorSubject(this.data);
+  }
 }
