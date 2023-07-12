f5c04876f3b2fc4174331f8d74f17045e85c7ac7
simphiwe-nonabe <106514948+simphiwe-nonabe@users.noreply.github.com>
2023-07-13 01:56:11 +0200

    ✨ (app) search feature library created

---
+++ libs/app/search/feature/src/search.page.spec.ts
@@ -0,0 +1,7 @@
+import { appSearchFeature } from './lib/app-search-feature';
+
+describe('appSearchFeature', () => {
+  it('should work', () => {
+    expect(appSearchFeature()).toEqual('app-search-feature');
+  });
+});
