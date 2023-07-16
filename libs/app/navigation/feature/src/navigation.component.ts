2559cf26dda52b728d644d18fe50395c4623f786
simphiwe-nonabe <106514948+simphiwe-nonabe@users.noreply.github.com>
2023-07-16 23:10:39 +0200

    ♻️ (app) changed href links to ngxs navigation

--- libs/app/navigation/feature/src/navigation.component.ts
+++ libs/app/navigation/feature/src/navigation.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { Select, Store } from '@ngxs/store';
+import { Navigate } from "@ngxs/router-plugin";
 
 @Component({
   selector: 'navigation-bar',
@@ -10,22 +12,31 @@ export class NavigationBar {
   @Output() notificationsClicked = new EventEmitter<void>();
   @Output() settingsClicked = new EventEmitter<void>();
 
-  constructor(private router: Router) {}
-
-  // isActive(pageName: string) {
-  //   if (this.router.url.includes('notifications')) {
-  //     return '';
-  //   }
-  //   else {
-  //   return this.router.url.includes(pageName) ? 'active' : '';
-  //   }
-  // }
+  constructor(private router: Router, private store: Store) {}
 
   isActive(pageName: string) {
-    if (pageName === 'profile') {
-      return this.router.url.includes(pageName) ? 'active' : '';
-    } else {
-      return this.router.url.includes(pageName) ? 'active' : '';
+    if (this.router.url.includes('notifications')) {
+      return 'active';
+    }
+    else {
+    return this.router.url.includes(pageName) ? 'active' : 'active';
     }
   }
+
+  openRecommend() {
+    this.store.dispatch(new Navigate(['/recommend']));
+  }
+
+  openSearch() {
+    alert("Search Page Not Implemented");
+  }
+
+  openCreate() {
+    this.store.dispatch(new Navigate(['/create']));
+  }
+
+  openProfile() {
+    this.store.dispatch(new Navigate(['/profile']));
+  }
+
 }
