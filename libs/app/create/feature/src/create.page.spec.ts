c17678abba97225a1ec146b56952afac2bb8fc26
Antwi-Antwi <u21448150@tuks.co.za>
2023-07-16 08:20:25 +0200

    🔥 (app) : refactored create files

--- libs/app/create/feature/src/create.page.spec.ts
+++ libs/app/create/feature/src/create.page.spec.ts
@@ -10,7 +10,7 @@ import { BehaviorSubject, take } from "rxjs";
 import { Injectable } from '@angular/core';
 import { NgxsModule, State, Store } from '@ngxs/store';
 import { IProfile } from '@fridge-to-plate/app/profile/utils';
-import { CreateRecipe } from '@fridge-to-plate/app/create/utils';
+import { CreateRecipe } from '@fridge-to-plate/app/recipe/utils';
 import { ShowError } from '@fridge-to-plate/app/error/utils';
 
 
