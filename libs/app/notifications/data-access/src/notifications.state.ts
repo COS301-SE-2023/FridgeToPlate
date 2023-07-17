2689ff79d0146c59deebe378b4aadda83cf7dba5
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-17 03:03:07 +0200

    feat: :sparkles: Added selectors to notifications state

---
+++ libs/app/notifications/data-access/src/notifications.state.ts
@@ -0,0 +1,30 @@
+import { Action, Selector, State } from '@ngxs/store';
+import { INotification } from '../../utils/src/interfaces';
+import { Injectable } from '@angular/core';
+import { NotificationsApi } from './notifications.api';
+
+export interface NotificationsStateModel {
+  generalNotifications: INotification[] | null;
+  recommendationNotification: INotification[] | null;
+}
+@State<NotificationsStateModel>({
+  name: 'notifications',
+  defaults: {
+    generalNotifications: [],
+    recommendationNotification: [],
+  },
+})
+@Injectable()
+export class NotificationsState {
+  constructor(notificationsApi: NotificationsApi) {}
+
+  @Selector()
+  static getGeneralNotifications(state: NotificationsStateModel) {
+    return state.generalNotifications;
+  }
+
+  @Selector()
+  static getRecommendationNotifications(state: NotificationsStateModel) {
+    return state.generalNotifications;
+  }
+}
