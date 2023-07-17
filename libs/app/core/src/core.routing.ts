018717448a17baa6847b9b7416034612abfb6208
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-17 02:53:43 +0200

    feat: :sparkles: Added actions and state files

--- libs/app/core/src/core.routing.ts
+++ libs/app/core/src/core.routing.ts
@@ -3,45 +3,55 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { RouteGuardService } from './guards/route-guard.service';
 
 const routes: Routes = [
-    {
-        path: '**',
-        pathMatch: 'full',
-        redirectTo: 'login',
-    },
-    {
-        path: 'recommend',
-        loadChildren: () => import('@fridge-to-plate/app/recommend/feature').then((m) => m.RecommendModule),
-        canActivate: [RouteGuardService]
-    },
-    {
-        path: 'login',
-        loadChildren: () => import('@fridge-to-plate/app/login/feature').then((m) => m.LoginModule),
-    },
-    {
-        path: 'signup',
-        loadChildren: () => import('@fridge-to-plate/app/signup/feature').then((m) => m.SignupModule),
-    },
-    {
-        path: 'profile',
-        loadChildren: () => import('@fridge-to-plate/app/profile/feature').then((m) => m.ProfileModule),
-        canActivate: [RouteGuardService]
-    },
-    {
-        path: 'create',
-        loadChildren: () => import('@fridge-to-plate/app/create/feature').then((m) => m.CreateModule),
-        canActivate: [RouteGuardService]
-    },
-    {
-        path: 'recipe',
-        loadChildren: () => import('@fridge-to-plate/app/recipe/feature').then((m) => m.RecipeModule),
-        canActivate: [RouteGuardService]
-    }
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'recommend',
+    loadChildren: () =>
+      import('@fridge-to-plate/app/recommend/feature').then(
+        (m) => m.RecommendModule
+      ),
+    canActivate: [RouteGuardService],
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('@fridge-to-plate/app/login/feature').then((m) => m.LoginModule),
+  },
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('@fridge-to-plate/app/signup/feature').then((m) => m.SignupModule),
+  },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('@fridge-to-plate/app/profile/feature').then(
+        (m) => m.ProfileModule
+      ),
+    canActivate: [RouteGuardService],
+  },
+  {
+    path: 'create',
+    loadChildren: () =>
+      import('@fridge-to-plate/app/create/feature').then((m) => m.CreateModule),
+    canActivate: [RouteGuardService],
+  },
+  {
+    path: 'recipe',
+    loadChildren: () =>
+      import('@fridge-to-plate/app/recipe/feature').then((m) => m.RecipeModule),
+    canActivate: [RouteGuardService],
+  },
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
-    ],
-    exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
 })
 export class CoreRouting {}
