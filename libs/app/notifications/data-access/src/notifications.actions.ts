103212ef49f158a8142a79e5829ad2df1e09c4f5
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-17 03:31:15 +0200

    feat: :construction: WIP: Added Notifications refresh action

--- libs/app/notifications/data-access/src/notifications.actions.ts
+++ libs/app/notifications/data-access/src/notifications.actions.ts
@@ -3,6 +3,16 @@ export class RefreshNotifications {
   constructor(public readonly userId: string) {}
 }
 
+export class RefreshGeneralNotifications {
+  static readonly type = '[Notifications] Refresh General Notifications';
+  constructor() {}
+}
+
+export class RefreshRecommendationNotifications {
+  static readonly type = '[Notifications] Refresh Recommendation Notifications';
+  constructor() {}
+}
+
 export class ClearNotifications {
   static readonly type = '[Notifications] Clear Notifications';
   constructor(public readonly userId: string) {}
