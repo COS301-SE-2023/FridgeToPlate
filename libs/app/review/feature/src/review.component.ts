7245bccd0bd4556d82a4f1f5851220b9449563db
simphiwe-nonabe <106514948+simphiwe-nonabe@users.noreply.github.com>
2023-07-18 22:41:42 +0200

    ♻️ (app) add review functionality

--- libs/app/review/feature/src/review.component.ts
+++ libs/app/review/feature/src/review.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'review',
@@ -10,11 +11,13 @@ export class Review {
   description = '';
   showReviews = false;
 
+  constructor (private store: Store) {}
+
   reviews = [
-    { rating: 4, description: 'Good stuff'},
-    { rating: 3, description: 'Nice'},
-    { rating: 5, description: 'Perfect'},
-    { rating: 1, description: 'Mediocre'},
+    { recipeId: 'abcd' , rating: 4, description: 'Good stuff'},
+    { recipeId: 'abcd' , rating: 3, description: 'Nice'},
+    { recipeId: 'abcn' , rating: 5, description: 'Perfect'},
+    { recipeId: 'abcw' , rating: 1, description: 'Mediocre'},
   ]
 
 
@@ -33,15 +36,16 @@ export class Review {
       return;
     }
 
+    this.store.dispatch(new AddReview());
 
-    const review = {
-      rating: this.rating,
-      description: this.description
-    };
+    // const review = {
+    //   rating: this.rating,
+    //   description: this.description
+    // };
 
-    this.reviews.unshift(review);
+    // this.reviews.unshift(review);
 
-    // send the review data to a server or store it locally
-    console.log(this.reviews);
+    // // send the review data to a server or store it locally
+    // console.log(this.reviews);
   }
 }
