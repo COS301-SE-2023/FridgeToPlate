8f51e24ccd16adc9a990c53a978195274fd467eb
Azola <azola.m.lukhozi@gmail.com>
2023-07-19 16:01:47 +0200

    ✨ (App) Create New Preference On SIgnup

--- libs/app/preferences/utils/src/preferences.actions.ts
+++ libs/app/preferences/utils/src/preferences.actions.ts
@@ -4,3 +4,8 @@ export class UpdatePreferences {
     static readonly type = '[Preferences] UpdatePreference';
     constructor(public readonly preferences: IPreferences) {}
 }
+
+export class CreateNewPreference {
+    static readonly type = '[Preferences] CreateNewPreference';
+    constructor(public readonly profile: IPreferences) {}
+}
