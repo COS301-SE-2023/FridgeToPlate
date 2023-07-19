628152d53070327bd73ab06ea16804f6cc744608
Azola <azola.m.lukhozi@gmail.com>
2023-07-19 19:54:05 +0200

    🚧 (App) Setup Preference Actions

--- libs/app/preferences/utils/src/preferences.actions.ts
+++ libs/app/preferences/utils/src/preferences.actions.ts
@@ -5,7 +5,16 @@ export class UpdatePreferences {
     constructor(public readonly preferences: IPreferences) {}
 }
 
-export class CreateNewPreference {
-    static readonly type = '[Preferences] CreateNewPreference';
+export class CreateNewPreferences {
+    static readonly type = '[Preferences] CreateNewPreferences';
     constructor(public readonly profile: IPreferences) {}
+}
+
+export class ResetPreferences {
+    static readonly type = '[Preferences] ResetPreferences';
+}
+
+export class RetrievePreferences {
+    static readonly type = '[Preferences] RetrievePreferences';
+    constructor(public readonly username: string) {}
 }
