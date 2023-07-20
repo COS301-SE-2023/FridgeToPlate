158b3c2c448f3680a91c9d6d1f6a155594a31f08
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-21 01:26:36 +0200

    feat: :sparkles: WIP: Added ExternalApiService to application

--- apps/api/src/main/java/com/fridgetoplate/controller/RecommendController.java
+++ apps/api/src/main/java/com/fridgetoplate/controller/RecommendController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import com.fridgetoplate.frontendmodels.RecipeFrontendModel;
 import com.fridgetoplate.repository.RecipeRepository;
+import com.fridgetoplate.service.ExternalApiService;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
@@ -15,13 +16,17 @@ public class RecommendController {
     @Autowired
     private RecipeRepository recipeRepository;
 
+    @Autowired
+    private ExternalApiService apiService;
+
     @GetMapping
     public List<RecipeFrontendModel> findAll() {
         return recipeRepository.findAll();
     }
 
     @PostMapping
-    public List<RecipeFrontendModel> getExternalRecommendation() {        
+    public List<RecipeFrontendModel> getExternalRecommendation() {
+        apiService.spoonacularRecipeSearch();
         return recipeRepository.findAll();
     }
 
--- apps/api/src/main/java/com/fridgetoplate/service/ExternalApiService.java
+++ apps/api/src/main/java/com/fridgetoplate/service/ExternalApiService.java
@@ -4,15 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fridgetoplate.frontendmodels.RecipePreferencesFrontendModel;
 import com.fridgetoplate.model.SpoonacularRecipeModel;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @Service
 public class ExternalApiService {
     
+    @Value("${spoonacular.baseUrl}")
+   private String spoonacularbaseUrl;
+    
     @Autowired
     private RestTemplate template = new RestTemplate();
-
-    public SpoonacularRecipeModel[] spoonacularRecipeSearch(){}
+    
+    public void spoonacularRecipeSearch(){
+        //return template.getForObject(null, null, null)
+        System.out.println("HELLO WORLD!");
+        System.out.println(spoonacularbaseUrl);
+    }
 }
