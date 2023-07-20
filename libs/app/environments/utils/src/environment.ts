3023265053852151313fa806d2265b513f3610e0
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-07-20 09:15:39 +0200

    🎨 (app) Fixed Typo

---
+++ libs/app/environments/utils/src/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    type: process.env['type'],
+    apiURL: process.env['apiURL'],
+    cognitoUserPoolId: process.env['cognitoUserPoolId'],
+    cognitoAppClientId: process.env['cognitoAppClientId']
+}
