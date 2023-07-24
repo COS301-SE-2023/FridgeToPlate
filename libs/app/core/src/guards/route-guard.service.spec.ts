feeeaab0fe11ce9e23717ba31a7239f1bde29155
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-07-24 14:28:08 +0200

    ✅  (app) Added Routing Guards Tests

--- libs/app/core/src/guards/route-guard.service.spec.ts
+++ libs/app/core/src/guards/route-guard.service.spec.ts
@@ -1,38 +1,90 @@
 import { TestBed } from '@angular/core/testing';
 import { RouteGuardService } from './route-guard.service';
 import { Router } from '@angular/router';
-import { AuthService } from '@fridge-to-plate/app/auth/data-access';
+import { IProfile } from '@fridge-to-plate/app/profile/utils';
+import { NgxsModule, State } from '@ngxs/store';
+import { Injectable } from '@angular/core';
 
 describe('RouteGuardService', () => {
+
+  const testProfile: IProfile = {
+    displayName: "John Doe",
+    username: "jdoe",
+    email: "jdoe@gmail.com",
+    savedRecipes: [],
+    ingredients: [],
+    profilePic: "image-url",
+    createdRecipes: [],
+    currMealPlan: null,
+  };
 
   let service: RouteGuardService;
   let navigateSpy: jest.SpyInstance;
   let router: Router;
-  let authService: AuthService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(RouteGuardService);
-    router = TestBed.inject(Router);
-    authService = TestBed.inject(AuthService);
-  });
 
   it('should be created', () => {
+    
+    @State({
+      name: 'profile',
+      defaults: {
+        profile: null
+      }
+    })
+    @Injectable()
+    class MockProfileState {}
+
+    TestBed.configureTestingModule({
+      imports: [NgxsModule.forRoot([MockProfileState])]
+    });
+
+    service = TestBed.inject(RouteGuardService);
+
     expect(service).toBeTruthy();
   });
 
   it('should remain be routed to login (No Login)', () => {
-    jest.spyOn(authService, 'isUserLoggedIn').mockReturnValue(false);
+    
+    @State({
+      name: 'profile',
+      defaults: {
+        profile: null
+      }
+    })
+    @Injectable()
+    class MockProfileState {}
+
+    TestBed.configureTestingModule({
+      imports: [NgxsModule.forRoot([MockProfileState])]
+    });
+
+    service = TestBed.inject(RouteGuardService);
+    router = TestBed.inject(Router);
 
     navigateSpy = jest.spyOn(router, 'parseUrl');
 
     expect(service).toBeTruthy();
     service.canActivate();
-    expect(navigateSpy).toHaveBeenCalledWith('login');
+    expect(navigateSpy).toHaveBeenCalledWith('unauthorised');
   });
 
   it('should navigate to all routes (Login provided)', () => {
-    jest.spyOn(authService, 'isUserLoggedIn').mockReturnValue(true);
+    
+    @State({
+      name: 'profile',
+      defaults: {
+        profile: testProfile
+      }
+    })
+    @Injectable()
+    class MockProfileState {}
+
+    TestBed.configureTestingModule({
+      imports: [NgxsModule.forRoot([MockProfileState])]
+    });
+
+    service = TestBed.inject(RouteGuardService);
+    router = TestBed.inject(Router);
+
     expect(service).toBeTruthy();
     expect(service.canActivate()).toBe(true);
   });
