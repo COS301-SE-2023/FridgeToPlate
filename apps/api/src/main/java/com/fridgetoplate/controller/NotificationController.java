598cede2cc591d3820601692670886afda4441c4
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-24 22:00:19 +0200

    feat: :sparkles: Added NotificationController

---
+++ apps/api/src/main/java/com/fridgetoplate/controller/NotificationController.java
@@ -0,0 +1,42 @@
+package com.fridgetoplate.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fridgetoplate.model.NotificationModel;
+import com.fridgetoplate.repository.NotificationsRepository;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = { RequestMethod.GET, RequestMethod.DELETE })
+@RequestMapping("/notifications")
+
+public class NotificationController {
+    
+    @Autowired
+    private NotificationsRepository notificationsRepository;
+
+    @GetMapping("/{userId}")
+    public List<NotificationModel> findAll(@PathVariable(value = "userId") String userId){
+        return notificationsRepository.findAll(userId);
+    }
+
+    @DeleteMapping("/{notificationId}")
+    public String delete(@PathVariable(value = "notificationId") String notificationId){
+        return notificationsRepository.delete(notificationId);
+    }
+
+    @DeleteMapping("/clear/{userId}")
+    public String clearNotifications(@PathVariable(value = "userId") String userId){
+        return notificationsRepository.clearNotifications(userId);
+    }
+
+}
