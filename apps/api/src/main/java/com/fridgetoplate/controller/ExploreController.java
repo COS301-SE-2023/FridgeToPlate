c6b78296d5006f418591337478641b8625af2812
Azola <azola.m.lukhozi@gmail.com>
2023-07-26 21:51:47 +0200

    🗃️ (Api) Added Controller For Searching

---
+++ apps/api/src/main/java/com/fridgetoplate/controller/ExploreController.java
@@ -0,0 +1,48 @@
+/**
+ * This is a Java class that defines the REST API endpoints for managing ingredients in a recipe
+ * application.
+ */
+package com.fridgetoplate.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fridgetoplate.interfaces.Explore;
+import com.fridgetoplate.interfaces.Recipe;
+import com.fridgetoplate.model.Ingredient;
+import com.fridgetoplate.model.MealPlanModel;
+import com.fridgetoplate.repository.IngredientRepository;
+import com.fridgetoplate.repository.MealPlanRepository;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
+@RequestMapping("/explore")
+
+public class ExploreController {
+    
+    @Autowired
+    private ExploreRepository exploreRepository;
+
+
+    @GetMapping
+    public List<Recipe> findAll() {
+        return exploreRepository.findAll();
+    }
+
+
+    @GetMapping("/refine")
+    public List<Recipe>  findBySearch(@RequestBody Explore search) {
+        return exploreRepository.findBySearch(search);
+    }
+}
