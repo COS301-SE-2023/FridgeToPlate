d88f1c5736fe1fc3aada04e6d72484a49c0ee2c4
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-29 18:32:01 +0200

    test: :test_tube: Updated test coverage

--- libs/app/recommend/ui/src/stepper-form/steper-form.spec.ts
+++ libs/app/recommend/ui/src/stepper-form/steper-form.spec.ts
@@ -4,17 +4,24 @@ import { IonicModule } from '@ionic/angular';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzStepsModule } from 'ng-zorro-antd/steps';
 import { RecipeUIModule } from '@fridge-to-plate/app/recipe/ui';
-import { RecommendDataAccessModule } from '@fridge-to-plate/app/recommend/data-access';
+import {
+  RecommendDataAccessModule,
+  RecommendState,
+} from '@fridge-to-plate/app/recommend/data-access';
 import { FormsModule } from '@angular/forms';
 import { ItemEditStep } from '../item-edit-step/item-edit-step';
 import { RecipeListStep } from '../recipe-list-step/recipe-list-step';
 import { RecipePreferencesStep } from '../recipe-preferences-step/recipe-preferences-step';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { NgxsModule } from '@ngxs/store';
-import {IngredientUIModule} from "@fridge-to-plate/app/ingredient/ui";
+import { NgxsModule, State, Store } from '@ngxs/store';
+import { IngredientUIModule } from '@fridge-to-plate/app/ingredient/ui';
+import { Injectable } from '@angular/core';
 describe('StepperForm', () => {
   let component: StepperForm;
   let fixture: ComponentFixture<StepperForm>;
+
+  let store: Store;
+  let dispatchSpy: jest.SpyInstance;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -33,7 +40,7 @@ describe('StepperForm', () => {
         FormsModule,
         IngredientUIModule,
         HttpClientTestingModule,
-        NgxsModule.forRoot()
+        NgxsModule.forRoot([RecommendState]),
       ],
     });
     fixture = TestBed.createComponent(StepperForm);
@@ -48,14 +55,18 @@ describe('StepperForm', () => {
   it('should create on first step', () => {
     expect(component.currentStep).toBe(1);
     expect(component.stepContent).toBe('Edit Your Ingredients');
-    expect(component.stepContentDesktop).toBe('Edit Your Ingredients and Preferences');
+    expect(component.stepContentDesktop).toBe(
+      'Edit Your Ingredients and Preferences'
+    );
   });
 
   it('should not go one step back when on first step', () => {
     component.previousStep();
     expect(component.currentStep).toBe(1);
     expect(component.stepContent).toBe('Edit Your Ingredients');
-    expect(component.stepContentDesktop).toBe('Edit Your Ingredients and Preferences');
+    expect(component.stepContentDesktop).toBe(
+      'Edit Your Ingredients and Preferences'
+    );
   });
 
   it('should go one step forward on nextStep', () => {
@@ -79,7 +90,9 @@ describe('StepperForm', () => {
 
     component.changeContent();
     expect(component.stepContent).toBe('Edit Your Ingredients');
-    expect(component.stepContentDesktop).toBe('Edit Your Ingredients and Preferences');
+    expect(component.stepContentDesktop).toBe(
+      'Edit Your Ingredients and Preferences'
+    );
   });
 
   it('changeContent should update step content', () => {
@@ -104,5 +117,27 @@ describe('StepperForm', () => {
     component.changeContent();
     expect(component.stepContent).toBe('error');
     expect(component.stepContentDesktop).toBe('error');
+  });
+
+  it('prevous step changes count', () => {
+    component.currentStep = 3;
+
+    component.previousStep();
+
+    expect(component.currentStep).toBeLessThan(3);
+  });
+
+  it('attempts a recommendation', () => {
+    store = TestBed.inject(Store);
+    dispatchSpy = jest.spyOn(store, 'dispatch');
+    const curerntStep = component.currentStep;
+    const currentContent = component.stepContent;
+    const currentContentDesktop = component.stepContent;
+
+    component.attemptRecommendation();
+
+    expect(dispatchSpy).toBeCalled();
+    expect(component.currentStep).toBeGreaterThan(curerntStep);
+    expect(component.stepContent).not.toBe(currentContent);
   });
 });
