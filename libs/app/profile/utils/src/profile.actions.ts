fed6f16ec6f50305644c51e7f3ffe6a23b5336fb
kphutiyagae <kopanophutiyagae@gmail.com>
2023-07-29 16:49:48 +0200

    Added update user ingredients action

--- libs/app/profile/utils/src/profile.actions.ts
+++ libs/app/profile/utils/src/profile.actions.ts
@@ -1,76 +1,85 @@
-import { IRecipeDesc } from "@fridge-to-plate/app/recipe/utils";
-import { IProfile } from "./interfaces";
-import { IMealPlan } from "@fridge-to-plate/app/meal-plan/utils";
+import { IRecipeDesc } from '@fridge-to-plate/app/recipe/utils';
+import { IProfile } from './interfaces';
+import { IMealPlan } from '@fridge-to-plate/app/meal-plan/utils';
+import { IIngredient } from '@fridge-to-plate/app/ingredient/utils';
 
 export class UpdateProfile {
-    static readonly type = '[Profile] UpdateProfile';
-    constructor(public readonly profile: IProfile) {}
+  static readonly type = '[Profile] UpdateProfile';
+  constructor(public readonly profile: IProfile) {}
 }
 
 export class ResetProfile {
-    static readonly type = '[Profile] ResetProfile';
+  static readonly type = '[Profile] ResetProfile';
 }
 
 export class CreateNewProfile {
-    static readonly type = '[Profile] CreateNewProfile';
-    constructor(public readonly profile: IProfile) {}
+  static readonly type = '[Profile] CreateNewProfile';
+  constructor(public readonly profile: IProfile) {}
 }
 
 export class RetrieveProfile {
-    static readonly type = '[Profile] RetrieveProfile';
-    constructor(public readonly username: string) {}
+  static readonly type = '[Profile] RetrieveProfile';
+  constructor(public readonly username: string) {}
 }
 
 export class SaveRecipe {
-    static readonly type = '[Profile] SaveRecipe';
-    constructor(public readonly recipe: IRecipeDesc) {}
+  static readonly type = '[Profile] SaveRecipe';
+  constructor(public readonly recipe: IRecipeDesc) {}
 }
 
 export class RemoveSavedRecipe {
-    static readonly type = '[Profile] RemoveSavedRecipe';
-    constructor(public readonly recipe: IRecipeDesc) {}
+  static readonly type = '[Profile] RemoveSavedRecipe';
+  constructor(public readonly recipe: IRecipeDesc) {}
 }
 
 export class UndoRemoveSavedRecipe {
-    static readonly type = '[Profile] UndoRemoveSavedRecipe';
-    constructor(public readonly savedRecipes: IRecipeDesc[]) {}
+  static readonly type = '[Profile] UndoRemoveSavedRecipe';
+  constructor(public readonly savedRecipes: IRecipeDesc[]) {}
 }
 
 export class SortSavedByDifficulty {
-    static readonly type = '[Profile] SortSavedByDifficulty';
+  static readonly type = '[Profile] SortSavedByDifficulty';
 }
 
 export class SortSavedByNameAsc {
-    static readonly type = '[Profile] SortSavedByNameAsc';
+  static readonly type = '[Profile] SortSavedByNameAsc';
 }
 
 export class SortSavedByNameDesc {
-    static readonly type = '[Profile] SortSavedByNameDesc';
+  static readonly type = '[Profile] SortSavedByNameDesc';
 }
 
 export class SortCreatedByDifficulty {
-    static readonly type = '[Profile] SortCreatedByDifficulty';
+  static readonly type = '[Profile] SortCreatedByDifficulty';
 }
 
 export class SortCreatedByNameAsc {
-    static readonly type = '[Profile] SortCreatedByNameAsc';
+  static readonly type = '[Profile] SortCreatedByNameAsc';
 }
 
 export class SortCreatedByNameDesc {
-    static readonly type = '[Profile] SortCreatedByNameDesc';
+  static readonly type = '[Profile] SortCreatedByNameDesc';
 }
 
 export class UpdateMealPlan {
-    static readonly type = '[Profile] Update the Meal Plan';
-    constructor(public readonly mealPlan: IMealPlan){}
+  static readonly type = '[Profile] Update the Meal Plan';
+  constructor(public readonly mealPlan: IMealPlan) {}
 }
 
 export class RemoveFromMealPlan {
-    static readonly type = '[Profile] Remove from Meal Plan';
-    constructor(public readonly recipeId: string){}
+  static readonly type = '[Profile] Remove from Meal Plan';
+  constructor(public readonly recipeId: string) {}
 }
 
 export class AddToMealPlan {
-    static readonly type = '[Profile] Add to Meal Plan';
-    constructor(public readonly recipe: IRecipeDesc, public readonly mealType: string){}
+  static readonly type = '[Profile] Add to Meal Plan';
+  constructor(
+    public readonly recipe: IRecipeDesc,
+    public readonly mealType: string
+  ) {}
+}
+
+export class UpdateUserIngredients {
+  static readonly type = '[Profile] Update User Ingredients';
+  constructor(public readonly updatedIngredientsList: IIngredient[]) {}
 }
