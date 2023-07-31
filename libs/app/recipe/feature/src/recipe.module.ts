a70e12b0a7c206bf373ad4772364d408f3912ed2
Antwi-Antwi <u21448150@tuks.co.za>
2023-07-31 11:58:43 +0200

    🐛 (app) : Added Missing import

--- libs/app/recipe/feature/src/recipe.module.ts
+++ libs/app/recipe/feature/src/recipe.module.ts
@@ -14,6 +14,7 @@ import { RecipeDataAccessModule } from '@fridge-to-plate/app/recipe/data-access'
   imports: [
     CommonModule,
     RecipeUIModule,
+    IonicModule,
     RecipeRouting,
     NavigationBarModule,
     ReviewModule,
