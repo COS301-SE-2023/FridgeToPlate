080cee5c09435346b5fcb3b80d7186e7db77fe57
kphutiyagae <kopanophutiyagae@gmail.com>
2023-09-05 01:54:16 +0200

    test: :test_tube: Added select-dropdown tests

--- libs/app/recommend/ui/src/dropdown-select/dropdown-select.component.spec.ts
+++ libs/app/recommend/ui/src/dropdown-select/dropdown-select.component.spec.ts
@@ -18,4 +18,39 @@ describe('DropdownSelectComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit newSearchEvent on explorer() call', () => {
+    const searchQuery = 'Test search query';
+    component.searchText = searchQuery;
+
+    // Call the filter() method
+    component.filterOptions();
+    // Subscribe to newSearchEvent EventEmitter
+    component.newSearchEvent.subscribe((searchTerm) => {
+      // Assert emitted search term
+      expect(searchTerm).toBe(searchQuery);
+    });
+  });
+
+  it('should toggle dropDownShowing to true', () => {
+    expect(component.isDropdownShowing).toBe(false);
+    component.showDropdown();
+    expect(component.isDropdownShowing).toBe(true);
+  });
+
+  it('should toggle dropDownShowing to false', () => {
+    component.isDropdownShowing = true;
+    component.hideDropdown();
+    expect(component.isDropdownShowing).toBe(false);
+  });
+
+  it('should emit selectedKeyword', () => {
+    const selectedKeyword = 'test';
+
+    component.onSelect('test');
+
+    component.selectedOption.subscribe((searchTerm) => {
+      expect(searchTerm).toBe(selectedKeyword);
+    });
+  });
 });
