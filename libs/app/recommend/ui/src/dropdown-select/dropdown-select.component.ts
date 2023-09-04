4f549e3df838c97924e13c01bc2c65a05a41e8e3
kphutiyagae <kopanophutiyagae@gmail.com>
2023-09-04 23:12:53 +0200

    feat: :construction: Added select functionality

--- libs/app/recommend/ui/src/dropdown-select/dropdown-select.component.ts
+++ libs/app/recommend/ui/src/dropdown-select/dropdown-select.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'fridge-to-plate-dropdown-select',
   templateUrl: './dropdown-select.component.html',
   styleUrls: ['./dropdown-select.component.css'],
 })
-export class DropdownSelectComponent {}
+export class DropdownSelectComponent {
+  @Input() selectOptions: string[];
+  @Input() placeholderText: string;
+  @Output() selectedOption: EventEmitter<string> = new EventEmitter();
+
+  public onSelect(option: string) {
+    this.selectedOption.emit(option);
+  }
+}
