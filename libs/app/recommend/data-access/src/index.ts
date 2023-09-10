54d6a5245d0f55ba51e36c737cb3076f3d79a87d
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-09-10 14:43:01 +0200

    ⚰️  (app) Cleaned Up Code

--- libs/app/recommend/data-access/src/index.ts
+++ libs/app/recommend/data-access/src/index.ts
@@ -1,4 +1,3 @@
 export * from './recommend.module';
 export * from './recommend.state';
-export * from './ingredients.mock';
 export * from './recommend.api';
--- libs/app/recommend/data-access/src/recommend.api.ts
+++ libs/app/recommend/data-access/src/recommend.api.ts
@@ -1,10 +1,7 @@
-import { ingredientsArray } from './ingredients.mock';
 import { IRecipe } from '@fridge-to-plate/app/recipe/utils';
-import { IIngredient } from '@fridge-to-plate/app/ingredient/utils';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, catchError, switchMap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { IProfile } from '@fridge-to-plate/app/profile/utils';
 import { IRecommend } from '@fridge-to-plate/app/recommend/utils';
 import { environment } from '@fridge-to-plate/app/environments/utils';
 
@@ -15,39 +12,7 @@ const baseUrl = environment.API_URL + '/recommend';
 })
 export class RecommendApi {
   constructor(private httpClient: HttpClient) {}
-  //Step 1
-  getUserIngredientsList(): Observable<IIngredient[]> {
-    const req: Observable<IIngredient[]> = this.httpClient
-      .get<IProfile>(`${baseUrl}profiles/9be7b531-4980-4d3b-beff-a35d08f2637e`)
-      .pipe(
-        switchMap((res: IProfile) => {
-          return new BehaviorSubject<IIngredient[]>(res.ingredients);
-        }),
-        catchError(async (error) => {
-          console.log('An error has occured: ', error);
-          return error;
-        })
-      );
 
-    return req;
-  }
-
-  removeIngredient(ingredient: IIngredient) {
-    return ingredientsArray.filter(
-      (ingredientItem) => ingredientItem.name !== ingredient.name
-    );
-  }
-
-  //Step 2
-  getDietList(): Observable<string[]> {
-    const dietList = ['Vegan', 'Vegetarian', 'Paleo-tonic', 'Ketogenic'];
-
-    const req = new BehaviorSubject<string[]>(dietList);
-
-    return req;
-  }
-
-  //Step 3
   getRecommendations(recomendationParams: IRecommend): Observable<IRecipe[]> {
     const req: Observable<IRecipe[]> = this.httpClient.post<IRecipe[]>(
       baseUrl,
