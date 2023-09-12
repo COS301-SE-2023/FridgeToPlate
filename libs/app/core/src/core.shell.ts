fc8c72b8f614294e13603b646e75b945a78e8dc8
kphutiyagae <kopanophutiyagae@gmail.com>
2023-09-12 21:45:47 +0200

    Added push notifications to prod application.

--- libs/app/core/src/core.shell.ts
+++ libs/app/core/src/core.shell.ts
@@ -1,12 +1,30 @@
 /* eslint-disable @angular-eslint/component-selector */
 /* eslint-disable @angular-eslint/component-class-suffix */
-import { Component } from "@angular/core";
-
+import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+import { environment } from '@fridge-to-plate/app/environments/utils';
 @Component({
-  selector: "core-shell",
-  templateUrl: "./core.shell.html",
-  styleUrls: ["./core.shell.scss"],
+  selector: 'core-shell',
+  templateUrl: './core.shell.html',
+  styleUrls: ['./core.shell.scss'],
 })
 export class CoreShell {
-  title = "FridgeToPlate";
+  title = 'FridgeToPlate';
+
+
+  subscribeToNotifications() {
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: environment.VAPIDPublicKey,
+      })
+      .then((sub: PushSubscription) => {
+        console.log(sub);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  constructor(private swPush: SwPush) {
+    this.subscribeToNotifications();
+  }
 }
--- libs/app/environments/utils/src/environment.ts
+++ libs/app/environments/utils/src/environment.ts
@@ -5,4 +5,6 @@ export const environment = {
   COGNITO_APP_CLIENT_ID: process.env['COGNITO_APP_CLIENT_ID'] || 'none',
   GOUPC_APIKEY: process.env['GOUPC_APIKEY'] || 'none',
   GOUPC_API_URL: process.env['GOUPC_API_URL'] || 'none',
+  VAPIDPublicKey: process.env['VAPIDPublicKey'] || 'none',
+  VAPIDPrivateKey: process.env['VAPIDPrivateKey'] || 'none',
 };
