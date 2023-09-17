0e3938e7d6254b2cb99d67ff8b1c01ea2e2a016a
kphutiyagae <kopanophutiyagae@gmail.com>
2023-09-17 23:02:51 +0200

    feat: :sparkles: Added all scheduled tasks for pushed notifications

--- apps/api/src/main/java/com/fridgetoplate/service/NotificationService.java
+++ apps/api/src/main/java/com/fridgetoplate/service/NotificationService.java
@@ -13,7 +13,7 @@ import com.fridgetoplate.model.NotificationModel;
 import com.fridgetoplate.repository.NotificationsRepository;
 import com.fridgetoplate.repository.ProfileRepository;
 import com.fridgetoplate.model.ProfileModel;
-
+import com.fridgetoplate.utils.NotificationsUtils;
 @Service
 public class NotificationService {
 
@@ -22,6 +22,8 @@ public class NotificationService {
 
     @Autowired
     private ProfileRepository profileRepository;
+
+    private NotificationsUtils utils;
 
     Random random = new Random();
 
@@ -70,12 +72,9 @@ public class NotificationService {
 
     }
 
-    @Scheduled(fixedRate = 5000)
-    public void execute() {
-        
+    @Scheduled(cron = "0 0 8 * * ?")
+    public void breakfastNotificationsPush(){
         random.setSeed(System.currentTimeMillis());
-
-        System.out.println("Code is being run....");
         
         List<ProfileModel> allUsers =  profileRepository.findAllUsers();
         List<ProfileModel> selectedUsers = new ArrayList();
@@ -100,11 +99,82 @@ public class NotificationService {
         newNotification.setUserId(currentUser.getUsername());
         newNotification.setNotificationPic("https://www.pngitem.com/pimgs/m/24-248366_profile-clipart-generic-user-generic-profile-picture-gender.png");
         newNotification.setType("recommendation");
-        newNotification.setTitle("Lunch is served, 'burrito'-tifully! 🌯🍴");
-        newNotification.setBody("Don't panic, we have you covered for your lunchtime");
+        newNotification.setTitle(utils.breakfastTitleList[random.nextInt(0, utils.breakfastTitleList.length - 1)]);
+        newNotification.setBody("Rise and shine!, a quick meal on the go or a full english breakfast - we have the recipe just for you.");
 
         System.out.println(newNotification.toString());
         this.save(newNotification);
+        }        
+    }
+
+    @Scheduled(cron = "0 0 12 * * ?")
+    public void lunchtimeNotificationsPush() {
+        
+        random.setSeed(System.currentTimeMillis());
+        
+        List<ProfileModel> allUsers =  profileRepository.findAllUsers();
+        List<ProfileModel> selectedUsers = new ArrayList();
+        
+        //1. Get random users
+        for(; selectedUsers.size() < allUsers.size() * 0.6 ;){
+            int index = random.nextInt(0, allUsers.size());
+            ProfileModel currentProfile = allUsers.get(index);
+            
+            if(!selectedUsers.contains(currentProfile)){
+                selectedUsers.add(currentProfile);
+            }
+        }
+
+        // 2. Create notifications
+        for(int i = 0; i < selectedUsers.size(); i++){
+        
+        ProfileModel currentUser = selectedUsers.get(i);
+
+        NotificationModel newNotification = new NotificationModel();
+         
+        newNotification.setUserId(currentUser.getUsername());
+        newNotification.setNotificationPic("https://www.pngitem.com/pimgs/m/24-248366_profile-clipart-generic-user-generic-profile-picture-gender.png");
+        newNotification.setType("recommendation");
+        newNotification.setTitle(utils.lunchtimeTitleList[random.nextInt(0, utils.lunchtimeTitleList.length - 1)]);
+        newNotification.setBody("Take a look at what we have cooking in our Recommendations");
+
+        this.save(newNotification);
         }
     }
+
+    @Scheduled(cron = "0 16 * * * ?")
+    public void dinnertimeNotificationsPush() {
+        
+        random.setSeed(System.currentTimeMillis());
+        
+        List<ProfileModel> allUsers =  profileRepository.findAllUsers();
+        List<ProfileModel> selectedUsers = new ArrayList();
+        
+        //1. Get random users
+        for(; selectedUsers.size() < allUsers.size() * 0.6 ;){
+            int index = random.nextInt(0, allUsers.size());
+            ProfileModel currentProfile = allUsers.get(index);
+            
+            if(!selectedUsers.contains(currentProfile)){
+                selectedUsers.add(currentProfile);
+            }
+        }
+
+        // 2. Create notifications
+        for(int i = 0; i < selectedUsers.size(); i++){
+        
+        ProfileModel currentUser = selectedUsers.get(i);
+
+        NotificationModel newNotification = new NotificationModel();
+         
+        newNotification.setUserId(currentUser.getUsername());
+        newNotification.setNotificationPic("https://www.pngitem.com/pimgs/m/24-248366_profile-clipart-generic-user-generic-profile-picture-gender.png");
+        newNotification.setType("recommendation");
+        newNotification.setTitle(utils.dinnertimeTitleList[random.nextInt(0, utils.dinnertimeTitleList.length - 1)]);
+        newNotification.setBody("We get it, it's been a long day. Take a look at some of our quick to prepare dinner recipes for your next meal!");
+
+        this.save(newNotification);
+        }
+    }
+
 }
