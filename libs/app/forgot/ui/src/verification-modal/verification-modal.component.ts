2328ed3fa48e1d4417ed3033311954abaa4164d2
Azola <azola.m.lukhozi@gmail.com>
2023-09-17 20:07:16 +0200

    🎨 (App) Updated Toast For Forgot Page

--- libs/app/forgot/ui/src/verification-modal/verification-modal.component.ts
+++ libs/app/forgot/ui/src/verification-modal/verification-modal.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { NewPassword } from '@fridge-to-plate/app/auth/utils';
 import { ShowError } from '@fridge-to-plate/app/error/utils';
+import { ShowInfo } from '@fridge-to-plate/app/info/utils';
 import { Store } from '@ngxs/store';
 
 @Component({
@@ -24,7 +25,7 @@ export class VerificationModalComponent {
 
     if (this.verification_code != "" && this.new_password != "" && this.confirm_password != "") {
       if (this.new_password != this.confirm_password) 
-        this.store.dispatch(new ShowError("Please Enter Matching Passwords"));
+        this.store.dispatch(new ShowInfo("Please Enter Matching Passwords"));
       
       else
         this.store.dispatch(new NewPassword(this.verification_code, this.new_password));
