27fa380b5a0db6ebdce7d15fe00ac82ae2ba5c0a
kphutiyagae <kopanophutiyagae@gmail.com>
2023-09-17 22:35:59 +0200

    feat: :sparkles: Notifications being saved on DB

--- apps/api/src/main/java/com/fridgetoplate/repository/ProfileRepository.java
+++ apps/api/src/main/java/com/fridgetoplate/repository/ProfileRepository.java
@@ -58,4 +58,5 @@ public class ProfileRepository {
 
         return reviews;
     }
+
 }
--- apps/api/src/main/java/com/fridgetoplate/service/NotificationService.java
+++ apps/api/src/main/java/com/fridgetoplate/service/NotificationService.java
@@ -2,19 +2,28 @@ package com.fridgetoplate.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.fridgetoplate.frontendmodels.NotificationsResponseModel;
 import com.fridgetoplate.model.NotificationModel;
 import com.fridgetoplate.repository.NotificationsRepository;
+import com.fridgetoplate.repository.ProfileRepository;
+import com.fridgetoplate.model.ProfileModel;
 
 @Service
 public class NotificationService {
 
     @Autowired
     private NotificationsRepository notificationsRepository;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    Random random = new Random();
 
     public NotificationModel save(NotificationModel notification){
         notificationsRepository.save(notification);
@@ -59,5 +68,43 @@ public class NotificationService {
         notificationsRepository.deleteAll(deletableNotifications);
         return "Successfully cleared all " + type + " notifications";
 
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void execute() {
+        
+        random.setSeed(System.currentTimeMillis());
+
+        System.out.println("Code is being run....");
+        
+        List<ProfileModel> allUsers =  profileRepository.findAllUsers();
+        List<ProfileModel> selectedUsers = new ArrayList();
+        
+        //1. Get random users
+        for(; selectedUsers.size() < allUsers.size() * 0.6 ;){
+            int index = random.nextInt(0, allUsers.size());
+            ProfileModel currentProfile = allUsers.get(index);
+            
+            if(!selectedUsers.contains(currentProfile)){
+                selectedUsers.add(currentProfile);
+            }
+        }
+
+        // 2. Create notifications
+        for(int i = 0; i < selectedUsers.size(); i++){
+        
+        ProfileModel currentUser = selectedUsers.get(i);
+
+        NotificationModel newNotification = new NotificationModel();
+         
+        newNotification.setUserId(currentUser.getUsername());
+        newNotification.setNotificationPic("https://www.pngitem.com/pimgs/m/24-248366_profile-clipart-generic-user-generic-profile-picture-gender.png");
+        newNotification.setType("recommendation");
+        newNotification.setTitle("Lunch is served, 'burrito'-tifully! 🌯🍴");
+        newNotification.setBody("Don't panic, we have you covered for your lunchtime");
+
+        System.out.println(newNotification.toString());
+        this.save(newNotification);
+        }
     }
 }
