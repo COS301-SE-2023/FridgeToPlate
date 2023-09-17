3cf37c165f34ada3dca45032a150409fcd393978
Azola <azola.m.lukhozi@gmail.com>
2023-09-17 11:23:33 +0200

    ✨ (App) Added ShowError Toast Inside Info State

--- libs/app/info/data-access/src/info.state.ts
+++ libs/app/info/data-access/src/info.state.ts
@@ -1,6 +1,6 @@
 import { Action, State, StateContext } from "@ngxs/store";
 import { Injectable } from '@angular/core';
-import { ShowInfo } from "@fridge-to-plate/app/info/utils";
+import { ShowInfo, ShowError } from "@fridge-to-plate/app/info/utils";
 import { ToastController } from "@ionic/angular";
 
 export interface InfoStateModel {
@@ -29,6 +29,22 @@ export class InfoState {
             message: info,
             color: 'medium-tint',
             duration: 2000,
+            position: 'bottom',
+        });
+
+        await toast.present();
+    }
+
+    @Action(ShowError)
+    async showError({ patchState } : StateContext<InfoStateModel>, { error }: ShowError) {
+        patchState({
+            info: error
+        });
+
+        const toast = await this.toastController.create({
+            message: error,
+            color: 'danger',
+            duration: 2500,
             position: 'bottom',
         });
 
