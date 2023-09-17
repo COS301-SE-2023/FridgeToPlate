50a9f071b5661f62d91daaa24d14049a1a74bfca
kphutiyagae <kopanophutiyagae@gmail.com>
2023-09-17 11:07:29 +0200

    feat: :sparkles: Added Web socket to angular application

--- libs/app/core/src/core.shell.ts
+++ libs/app/core/src/core.shell.ts
@@ -3,6 +3,8 @@
 import { Component } from '@angular/core';
 import { SwPush } from '@angular/service-worker';
 import { environment } from '@fridge-to-plate/app/environments/utils';
+import { from, map, of } from 'rxjs';
+import { WebSocketService } from './services/web-socket.service';
 @Component({
   selector: 'core-shell',
   templateUrl: './core.shell.html',
@@ -11,20 +13,25 @@ import { environment } from '@fridge-to-plate/app/environments/utils';
 export class CoreShell {
   title = 'FridgeToPlate';
 
+  subscribeToNotificationsPromise = this.swPush
+    .requestSubscription({
+      serverPublicKey: environment.VAPIDPublicKey,
+    })
+    .then((sub: PushSubscription) => {
+      console.log(sub);
+      //Send to backend
+      return sub;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-  subscribeToNotifications() {
-    this.swPush
-      .requestSubscription({
-        serverPublicKey: environment.VAPIDPublicKey,
-      })
-      .then((sub: PushSubscription) => {
-        console.log(sub);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  constructor(private swPush: SwPush) {
-    this.subscribeToNotifications();
+  constructor(
+    private swPush: SwPush,
+    private webSocketService: WebSocketService
+  ) {
+    from(this.subscribeToNotificationsPromise).pipe(
+      map((pushSubsriptionResponse) => {})
+    );
   }
 }
--- libs/app/core/src/services/web-socket.service.ts
+++ libs/app/core/src/services/web-socket.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '@fridge-to-plate/app/environments/utils';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WebSocketService {
+  private socket$: WebSocketSubject<any>;
 
-  constructor() { }
+  constructor() {
+    this.socket$ = webSocket(
+      `${
+        environment.TYPE === 'dev'
+          ? environment.API_WS_URL_DEV
+          : environment.API_WS_URL_PROD
+      }`
+    );
+  }
+
+  getSocket() {
+    return this.socket$;
+  }
 }
