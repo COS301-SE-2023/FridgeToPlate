24fc43857df0f1d9c2b483e435772c045dba5149
Azola <azola.m.lukhozi@gmail.com>
2023-09-17 11:19:59 +0200

    🚧 (App) Added ShowError Action For Info Toast

--- libs/app/info/utils/src/info.actions.ts
+++ libs/app/info/utils/src/info.actions.ts
@@ -1,5 +1,9 @@
 export class ShowInfo {
     static readonly type = '[Infos] ShowInfo';
     constructor(public readonly info: string) {}
-  }
-  
+}
+
+export class ShowError {
+  static readonly type = '[Errors] ShowError';
+  constructor(public readonly error: string) {}
+}
