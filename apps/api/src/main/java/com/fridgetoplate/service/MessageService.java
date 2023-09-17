83b2cf442ce7637a7f285dcc534058d95553e958
kphutiyagae <kopanophutiyagae@gmail.com>
2023-09-17 05:04:24 +0200

    Added packages and created message service.

---
+++ apps/api/src/main/java/com/fridgetoplate/service/MessageService.java
@@ -0,0 +1,77 @@
+package com.fridgetoplate.service;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.Security;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.jose4j.lang.JoseException;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Value;
+import nl.martijndwars.webpush.Notification;
+import nl.martijndwars.webpush.PushService;
+import nl.martijndwars.webpush.Subscription;
+
+public class MessageService {
+ @Value("{VAPIDPublicKey}")
+ private String vapidPublicKey;
+
+ @Value("{VAPIDPrivateKey}")
+ private String vapidPrivateKey;
+
+ private PushService pushService;
+
+ private List<Subscription> subscriptions = new ArrayList<>();
+
+ @PostConstruct
+ private void init() throws GeneralSecurityException {
+    Security.addProvider(new BouncyCastleProvider());
+    pushService = new PushService(vapidPublicKey, vapidPrivateKey);
+ }
+
+ public String getPublicKey(){
+    return this.vapidPublicKey;
+ }
+
+ public void subscribe(Subscription sub){
+    System.out.println("Subscribed to " + sub.endpoint);
+    this.subscriptions.add(sub);
+ }
+
+ public void unsubscribe(String endpoint) {
+    System.out.println("Unsubscribed from " + endpoint);
+    subscriptions = subscriptions.stream().filter(s -> !endpoint.equals(s.endpoint))
+        .collect(Collectors.toList());
+  }
+
+  public void sendNotification(Subscription subscription, String messageJson) {
+    try {
+      pushService.send(new Notification(subscription, messageJson));
+    } catch (GeneralSecurityException |IOException | JoseException | ExecutionException | InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Scheduled(fixedRate = 15000)
+  private void sendNotifications() {
+    System.out.println("Sending notifications to all subscribers");
+
+    var json = """
+        {
+          "title": "Server says hello!",
+          "body": "It is now: %s"
+        }
+        """;
+
+    subscriptions.forEach(subscription -> {
+      sendNotification(subscription, String.format(json, LocalTime.now()));
+    });
+  }
+}
