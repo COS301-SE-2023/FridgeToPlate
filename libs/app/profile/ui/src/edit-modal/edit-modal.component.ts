d84b9f4a7e5296e0e28b5ae27e3d7deedc89a234
Azola <azola.m.lukhozi@gmail.com>
2023-09-24 19:39:11 +0200

    🚧 (App) Allow Saving Of Uploaded Image

--- libs/app/profile/ui/src/edit-modal/edit-modal.component.ts
+++ libs/app/profile/ui/src/edit-modal/edit-modal.component.ts
@@ -11,20 +11,28 @@ export class EditModalComponent {
   @Output() closeFunc: EventEmitter<any> = new EventEmitter();
   @Output() saveFunc: EventEmitter<any> = new EventEmitter();
   @Input() editableProfile !: IProfile;
+  profileImage : string;
 
   close() {
     this.closeFunc.emit();
   }
 
   save() {
+    this.editableProfile.profilePic = this.profileImage;
     this.saveFunc.emit();
     this.closeFunc.emit();
   }
 
   onProfileChanged(event: any){
     const file = event.target.files[0];
-    const fileSize = event.target.files[0].size;
 
-    return;
+    const reader = new FileReader();
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    reader.onload = (e: any) => {
+      this.profileImage = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
   }
 }
