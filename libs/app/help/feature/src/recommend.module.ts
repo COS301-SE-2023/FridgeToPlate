4d0e4fbe09c6eee3f55fdffc89f3fef26219231d
Azola <azola.m.lukhozi@gmail.com>
2023-09-28 18:21:03 +0200

    💩 (App) Added Skeleton Files For Help

---
+++ libs/app/help/feature/src/recommend.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecommendRouting } from './recommend.routing';
+import { IonicModule } from '@ionic/angular';
+import { NzStepsModule } from 'ng-zorro-antd/steps';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+import { RecommendPage } from './recommend.page';
+import { NavigationBarModule } from '@fridge-to-plate/app/navigation/feature';
+import { RecommendDataAccessModule } from '@fridge-to-plate/app/recommend/data-access';
+import { RecommendUIModule } from '@fridge-to-plate/app/recommend/ui';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RecommendRouting,
+    IonicModule,
+    NzStepsModule,
+    NzListModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzIconModule,
+    RecommendUIModule,
+    RecommendDataAccessModule,
+    NavigationBarModule,
+  ],
+  declarations: [RecommendPage],
+  exports: [RecommendPage],
+})
+export class RecommendModule {}
---
+++ libs/app/help/feature/src/recommend.page.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecommendPage } from './recommend.page';
+import { IonicModule } from '@ionic/angular';
+import { NavigationBarModule } from '@fridge-to-plate/app/navigation/feature';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { RecommendUIModule } from '@fridge-to-plate/app/recommend/ui';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxsModule } from '@ngxs/store';
+
+describe('RecipeRecommendationPage', () => {
+  let component: RecommendPage;
+  let fixture: ComponentFixture<RecommendPage>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RecommendPage],
+      imports: [
+        IonicModule, 
+        NavigationBarModule, 
+        RecommendUIModule,
+        HttpClientModule,
+        NgxsModule.forRoot(),
+      ],
+    });
+    fixture = TestBed.createComponent(RecommendPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
