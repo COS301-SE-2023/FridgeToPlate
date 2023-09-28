548f705afe413dd487960e4fdbbf84f62cd7761c
Azola <azola.m.lukhozi@gmail.com>
2023-09-28 18:28:21 +0200

    🎨 (App) Fixed Help Module Code

--- libs/app/help/feature/src/help.module.ts
+++ libs/app/help/feature/src/help.module.ts
@@ -1,35 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RecommendRouting } from './help.routing';
-import { IonicModule } from '@ionic/angular';
-import { NzStepsModule } from 'ng-zorro-antd/steps';
-import { NzListModule } from 'ng-zorro-antd/list';
-import { ReactiveFormsModule } from '@angular/forms';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { HelpRouting } from './help.routing';
+import { HelpPage } from './help.page';
 
-import { RecommendPage } from './help.page';
-import { NavigationBarModule } from '@fridge-to-plate/app/navigation/feature';
-import { RecommendDataAccessModule } from '@fridge-to-plate/app/recommend/data-access';
-import { RecommendUIModule } from '@fridge-to-plate/app/recommend/ui';
 
 @NgModule({
   imports: [
-    CommonModule,
-    RecommendRouting,
-    IonicModule,
-    NzStepsModule,
-    NzListModule,
-    ReactiveFormsModule,
-    NzFormModule,
-    NzInputModule,
-    NzIconModule,
-    RecommendUIModule,
-    RecommendDataAccessModule,
-    NavigationBarModule,
+    HelpRouting,
   ],
-  declarations: [RecommendPage],
-  exports: [RecommendPage],
+  declarations: [HelpPage],
+  exports: [HelpPage],
 })
-export class RecommendModule {}
+export class HelpModule {}
